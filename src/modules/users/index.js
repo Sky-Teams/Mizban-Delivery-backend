a0fa51e4a28473bfcac93c6279502aa4b92b594f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-18 20:35:35 +0430

    Update comment. #1

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,4 +1,4 @@
 // Export all files of this module from this file.
-// Example: => export { login } from './services/v1/user.service.js';
+// Example: export { login } from './services/v1/user.service.js';
 
 export { default as UserModel } from './models/user.model.js';
