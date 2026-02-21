2c15cc9838e80680f4a5fba2bea74089073eb54c
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-21 12:08:29 +0430

    Create login function in services #4

---
+++ src/modules/users/services/v1/auth.service.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+import { UserModel } from '../../models/user.model.js';
+import { generateAccessToken } from '../../../../shared/utils/jwt.js';
+
+export const login = async ({ email, password }) => {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) throw new Error('User not found');
+  if (!user.isActive) throw new Error('Account is disabled!');
+
+  const psMatch = await bcrypt.compare(password, user.password);
+  if (!psMatch) throw new Error('Incorrect Password');
+
+  const token = generateAccessToken({
+    id: user._id.toString(),
+    email: user.email,
+  });
+
+  return {
+    id: user._id,
+    email: user.email,
+    token,
+  };
+};
