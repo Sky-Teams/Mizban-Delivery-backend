d107fafeedeb1dbf98f26042ad6cb87bf1363bd2
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 05:34:32 +0430

    Create token helper functions #9

--- src/shared/utils/jwt.js
+++ src/shared/utils/jwt.js
@@ -1,5 +1,20 @@
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 export const verifyJWT = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+export const generateAccessToken = (user) => {
+  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+export const generateRefreshToken = () => {
+  return crypto.randomBytes(40).toString('hex');
+};
+
+export const hashToken = (token) => {
+  return crypto.createHash('sha256').update(token).digest('hex');
 };
