a14c73fd9efbe733df6f9b1e117ab1a7bcc53ebd
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-23 02:27:55 +0430

    use route in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { corsOptions } from './config/cors.js';
 import { errorHandler } from './shared/middleware/errorHandler.js';
+import authRoutesV1 from './modules/users/routes/v1/auth.routes.js';
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Public routes
+app.use('/api/v1/auth', authRoutesV1);
 
 // Protected routes
 
