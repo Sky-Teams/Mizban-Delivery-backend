39c4c69f466c192af69fdbd59dcee07acc5ad61f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-22 15:43:59 +0430

    Implement services for drivder. #14

---
+++ src/modules/drivers/services/v1/driver.service.js
@@ -0,0 +1,31 @@
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+import { AppError } from '#shared/errors/error.js';
+import { DriverModel } from '../../index.js';
+
+// Return Boolean value
+export const doesDriverExist = async (userId) => {
+  const exist = await DriverModel.exists({ user: userId });
+  return !!exist;
+};
+
+export const createNewDriver = async (userId, driverData) => {
+  const exists = await DriverModel.exists({ user: userId });
+  if (exists) throw new AppError('Driver already exist', 400, ERROR_CODES.DRIVER_ALREADY_EXIST);
+
+  const { vehicleType, status, capacity } = driverData;
+  const { maxWeightKg, maxPackages } = capacity;
+
+  const newDriver = {
+    user: userId,
+    vehicleType,
+    status,
+    capacity: {
+      maxWeightKg,
+      maxPackages,
+    },
+  };
+
+  const driver = await DriverModel.create(newDriver);
+
+  return driver;
+};
