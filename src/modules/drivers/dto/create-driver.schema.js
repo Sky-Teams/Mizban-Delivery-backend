7153b7225ba010ef4bcdff3923debe5acffc7ff1
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-22 17:10:38 +0430

    Add scheam validation for creating a delivery. #14

---
+++ src/modules/drivers/dto/create-driver.schema.js
@@ -0,0 +1,32 @@
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+import { z } from 'zod';
+
+const vehicleTypes = ['bike', 'car', 'van'];
+const driverStatuses = ['offline', 'idle', 'assigned', 'delivering'];
+
+const createDriverSchema = z.object({
+  body: z.object({
+    vehicleType: z.enum(vehicleTypes, {
+      errorMap: () => ({ message: ERROR_CODES.INVALID_VEHICLE_TYPE }),
+    }),
+    status: z
+      .enum(driverStatuses, { errorMap: () => ({ message: ERROR_CODES.INVALID_STATUS }) })
+      .optional(),
+    capacity: z
+      .object({
+        maxWeightKg: z.coerce
+          .number({ invalid_type_error: ERROR_CODES.MAX_WEIGHT_MUST_BE_NUMBER })
+          .positive({ message: ERROR_CODES.MAX_WEIGHT_MUST_BE_POSITIVE }),
+
+        maxPackages: z.coerce
+          .number({ invalid_type_error: ERROR_CODES.MAX_PACKAGES_MUST_BE_NUMBER })
+          .int({ message: ERROR_CODES.MAX_PACKAGES_MUST_BE_INTEGER })
+          .positive({ message: ERROR_CODES.MAX_PACKAGES_MUST_BE_POSITIVE }),
+      })
+      .optional(),
+  }),
+});
+
+export const createDriverValidator = (req) => {
+  return createDriverSchema.safeParse({ body: req.body });
+};
