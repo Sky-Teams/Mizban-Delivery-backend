d3e3545faf14c9e6b034db91ee69c1126cf93986
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 16:40:47 +0430

    Fix cookie settings #9

--- src/modules/users/controllers/v1/auth.controller.js
+++ src/modules/users/controllers/v1/auth.controller.js
@@ -1,7 +1,7 @@
 import { loginService, refreshService } from '../../services/v1/auth.service.js';
 
 export const refreshAccessToken = async (req, res) => {
-  const { accessToken } = refreshService(req.cookies.refreshToken);
+  const { accessToken } = await refreshService(req.cookies.refreshToken);
 
   res.status(200).json({
     success: true,
@@ -16,8 +16,8 @@ export const login = async (req, res) => {
   const { accessToken, refreshToken, id, email } = await loginService(req.body);
 
   res.cookie('refreshToken', refreshToken, {
-    httpOnly,
-    secure: true,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
   });
 
