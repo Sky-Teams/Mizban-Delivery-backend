1e1dd41b8bc4b80e7c50388ce25f8105d28d23e5
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 06:38:22 +0430

    implement refresh token in login #9

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,7 +1,31 @@
 import bcrypt from 'bcryptjs';
-import { generateAccessToken, hashToken } from '../../../../shared/utils/jwt.js';
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  hashToken,
+} from '../../../../shared/utils/jwt.js';
 import { RefreshTokenModel } from '../../models/refreshToken.model.js';
 import { UserModel } from '../../models/user.model.js';
+
+export const loginService = async ({ email, password }) => {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) throw new Error('User not found');
+  if (!user.isActive) throw new Error('Account is disabled!');
+
+  const psMatch = await bcrypt.compare(password, user.password);
+  if (!psMatch) throw new Error('Incorrect Password');
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken();
+
+  return {
+    id: user._id,
+    email: user.email,
+    accessToken,
+    refreshToken,
+  };
+};
 
 export const refreshService = async (refreshCookie) => {
   if (!refreshCookie) throw new Error('Token not found');
@@ -18,25 +42,4 @@ export const refreshService = async (refreshCookie) => {
   const newAccessToken = generateAccessToken(storedToken.user);
 
   return { accessToken: newAccessToken };
-};
-
-export const login = async ({ email, password }) => {
-  const user = await UserModel.findOne({ email });
-
-  if (!user) throw new Error('User not found');
-  if (!user.isActive) throw new Error('Account is disabled!');
-
-  const psMatch = await bcrypt.compare(password, user.password);
-  if (!psMatch) throw new Error('Incorrect Password');
-
-  const token = generateAccessToken({
-    id: user._id.toString(),
-    email: user.email,
-  });
-
-  return {
-    id: user._id,
-    email: user.email,
-    token,
-  };
 };
