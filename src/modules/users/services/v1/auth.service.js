35126c84a3b578579a6938948714aff2c73ff9dc
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 16:41:15 +0430

    Use AppError #9

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,18 +1,22 @@
 import bcrypt from 'bcryptjs';
 import { UserModel } from '../../models/user.model.js';
 import { RefreshTokenModel } from '../../models/refreshToken.model.js';
-import { generateAccessToken } from '../../../../shared/utils/jwt.js';
-import { AppError } from '../../../../shared/errors/error.js';
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  hashToken,
+} from '../../../../shared/utils/jwt.js';
+import { AppError, notFound } from '../../../../shared/errors/error.js';
 import { ERROR_CODES } from '../../../../shared/errors/customCodes.js';
 
 export const loginService = async ({ email, password }) => {
   const user = await UserModel.findOne({ email });
 
-  const psMatch = await bcrypt.compare(password, user.password);
-  if (!user || !psMatch)
-    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
-
+  if (!user) throw notFound('User');
   if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
+
+  const psMatch = await bcrypt.compare(password, user.password);
+  if (!psMatch) throw new AppError('Invalid password', 401, ERROR_CODES.INVALID_CREDENTIAL);
 
   const accessToken = generateAccessToken(user);
   const refreshToken = generateRefreshToken();
@@ -20,7 +24,7 @@ export const loginService = async ({ email, password }) => {
   await RefreshTokenModel.create({
     user: user._id,
     token: hashToken(refreshToken),
-    expireAt: new Date(Date.now() + 7 * 60 * 60 * 1000),
+    expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   });
 
   return {
@@ -32,15 +36,21 @@ export const loginService = async ({ email, password }) => {
 };
 
 export const refreshService = async (refreshCookie) => {
-  if (!refreshCookie) throw new Error('Token not found');
+  if (!refreshCookie)
+    throw new AppError('Unauthorized: Cookie not found', 401, ERROR_CODES.INVALID_CREDENTIAL);
 
-  const hashToken = hashToken(refreshCookie);
-  const storedToken = await RefreshTokenModel.findOne({ token: hashToken });
+  const hashedToken = hashToken(refreshCookie);
+  const storedToken = await RefreshTokenModel.findOne({ token: hashedToken }).populate(
+    'user',
+    '_id, email'
+  );
 
-  if (!storedToken) throw new Error('Token not found');
+  if (!storedToken)
+    throw new AppError('Unauthorized: Token not found', 401, ERROR_CODES.INVALID_CREDENTIAL);
+
   if (storedToken.expireAt < new Date()) {
     await storedToken.deleteOne();
-    throw new Error('Token not found');
+    throw new AppError('Unauthorized: Token expired', 401, ERROR_CODES.INVALID_CREDENTIAL);
   }
 
   const newAccessToken = generateAccessToken(storedToken.user);
