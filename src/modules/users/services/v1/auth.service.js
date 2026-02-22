3ccbeda16a21f194ae6e722b1c706acf9e93c066
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 12:06:00 +0430

    Add an error code and also use AppError #4

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,15 +1,17 @@
 import bcrypt from 'bcryptjs';
 import { UserModel } from '../../models/user.model.js';
 import { generateAccessToken } from '../../../../shared/utils/jwt.js';
+import { AppError, notFound } from '../../../../shared/errors/error.js';
+import { ERROR_CODES } from '../../../../shared/errors/customCodes.js';
 
 export const loginService = async ({ email, password }) => {
   const user = await UserModel.findOne({ email });
 
-  if (!user) throw new Error('User not found');
-  if (!user.isActive) throw new Error('Account is disabled!');
+  if (!user) throw notFound('User');
+  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
 
   const psMatch = await bcrypt.compare(password, user.password);
-  if (!psMatch) throw new Error('Incorrect Password');
+  if (!psMatch) throw new AppError('Invalid credential', 400, ERROR_CODES.INVALID_CREDENTIAL);
 
   const token = generateAccessToken({
     id: user._id.toString(),
