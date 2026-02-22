825450c831084e60e82ac0b182306af807758c4b
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-22 15:44:31 +0430

    Add Driver schema. #14

---
+++ src/modules/drivers/models/driver.model.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const DriverSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    vehicleType: {
+      type: String,
+      enum: ['bike', 'car', 'van'],
+      default: 'bike',
+    },
+    status: {
+      type: String,
+      enum: ['offline', 'idle', 'assigned', 'delivering'],
+      default: 'offline',
+    },
+    capacity: {
+      maxWeightKg: { type: Number, required: true },
+      maxPackages: { type: Number, required: true },
+    },
+    currentLocation: {
+      type: { type: String, enum: ['Point'], default: 'Point' },
+      coordinates: { type: [Number], default: [0, 0] },
+    },
+    lastLocationAt: {
+      type: Date,
+      default: null,
+    },
+
+    ratingAvg: {
+      type: Number,
+      default: 0,
+    },
+    ratingCount: {
+      type: Number,
+      default: 0,
+    },
+    acceptanceRate: {
+      type: Number,
+      default: 0,
+    },
+
+    isVerified: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+DriverSchema.index({ currentLocation: '2dsphere' });
+
+export const DriverModel = mongoose.model('Driver', DriverSchema);
