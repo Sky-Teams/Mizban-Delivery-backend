43a513a91a5c1922e90f51729be41442dd5851dc
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 05:36:07 +0430

    Create a refresh token model #9

---
+++ src/modules/users/models/refreshToken.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const RefreshTokenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  expireAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+export const RefreshTokenModel = mongoose.model('RefreshToken', RefreshTokenSchema);
