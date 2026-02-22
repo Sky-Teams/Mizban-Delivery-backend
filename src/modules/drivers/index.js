0b2a80e4672441c549f873b77096b459d16e9df8
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-22 15:42:49 +0430

    Create central file for importing. #14

---
+++ src/modules/drivers/index.js
@@ -0,0 +1,4 @@
+export { DriverModel } from './models/driver.model.js';
+export { createDriver } from './controllers/v1/driver.controller.js';
+export { createNewDriver, doesDriverExist } from './services/v1/driver.service.js';
+export { default as driverRoutes } from './routes/v1/driver.routes.js';
