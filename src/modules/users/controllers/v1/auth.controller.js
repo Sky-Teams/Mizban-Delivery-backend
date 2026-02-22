361c7e01407be11130f07c5ed4a8b9aae31ba757
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 06:53:08 +0430

    Add cookie to login #9

--- src/modules/users/controllers/v1/auth.controller.js
+++ src/modules/users/controllers/v1/auth.controller.js
@@ -1,3 +1,5 @@
+import { loginService, refreshService } from '../../services/v1/auth.service.js';
+
 export const refreshAccessToken = async (req, res) => {
   const { accessToken } = refreshService(req.cookies.refreshToken);
 
@@ -9,13 +11,18 @@ export const refreshAccessToken = async (req, res) => {
 
 // console.log(new Date());    2026-02-21T15:25:37.569Z
 // console.log(Date.now());    1771687537628
-import { login } from '../../services/v1/auth.service.js';
 
-export const loginController = async (req, res) => {
-  const user = await login(req.body);
+export const login = async (req, res) => {
+  const { accessToken, refreshToken, id, email } = await loginService(req.body);
+
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly,
+    secure: true,
+    sameSite: 'strict',
+  });
 
   res.status(200).json({
     success: true,
-    data: user,
+    data: { accessToken, id, email },
   });
 };
