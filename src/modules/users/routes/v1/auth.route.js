1d1741fe76330bc265d3d95d78205f701d4ebd6a
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 12:05:24 +0430

    Add asyncHandler to route #4

--- src/modules/users/routes/v1/auth.route.js
+++ src/modules/users/routes/v1/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { login } from '../../controllers/v1/auth.controller.js';
+import { asyncHandler } from '../../../../shared/middleware/asyncHandler.js';
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post('/login', asyncHandler(login));
 
 export default router;
