9fe110c0ae56bdea7145a4e083bb6b0c004d7c1f
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 07:09:42 +0430

    Re-export at index #9

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -4,3 +4,10 @@ export { UserModel } from './models/user.model.js';
 export { login } from './services/v1/user.service.js';
 export { refreshService } from './services/v1/auth.service.js';
 export { refreshAccessToken } from './controllers/v1/auth.controller.js';
+export {
+  hashToken,
+  generateAccessToken,
+  generateRefreshToken,
+  verifyJWT,
+} from '../../shared/utils/jwt.js';
+export { RefreshTokenModel } from './models/refreshToken.model.js';
