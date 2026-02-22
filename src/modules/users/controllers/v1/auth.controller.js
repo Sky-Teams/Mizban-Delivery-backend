f7ba701cba5649347019fc50d4939dbfd600a12e
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-23 02:18:57 +0430

    user register service in controller

---
+++ src/modules/users/controllers/v1/auth.controller.js
@@ -0,0 +1,11 @@
+import { registerService } from '../../services/v1/auth.service.js';
+
+export const register = async (req, res) => {
+  const data = req.body;
+  await registerService(data);
+
+  res.status(200).json({
+    success: true,
+    message: 'User register successfully',
+  });
+};
