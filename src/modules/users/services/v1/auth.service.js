a73019657330174357d88f5d9680b6f54a5bcc0c
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-22 12:21:17 +0430

    Fix errors #4

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,22 +1,19 @@
 import bcrypt from 'bcryptjs';
 import { UserModel } from '../../models/user.model.js';
 import { generateAccessToken } from '../../../../shared/utils/jwt.js';
-import { AppError, notFound } from '../../../../shared/errors/error.js';
+import { AppError } from '../../../../shared/errors/error.js';
 import { ERROR_CODES } from '../../../../shared/errors/customCodes.js';
 
 export const loginService = async ({ email, password }) => {
   const user = await UserModel.findOne({ email });
 
-  if (!user) throw notFound('User');
+  const psMatch = await bcrypt.compare(password, user.password);
+  if (!user || !psMatch)
+    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
+
   if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
 
-  const psMatch = await bcrypt.compare(password, user.password);
-  if (!psMatch) throw new AppError('Invalid credential', 400, ERROR_CODES.INVALID_CREDENTIAL);
-
-  const token = generateAccessToken({
-    id: user._id.toString(),
-    email: user.email,
-  });
+  const token = generateAccessToken(user);
 
   return {
     id: user._id,
--- src/shared/utils/jwt.js
+++ src/shared/utils/jwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-export const generateAccessToken = ({ id, email }) => {
-  return jwt.sign({ id, email }, process.env.JWT_SECRET, {
+export const generateAccessToken = (user) => {
+  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
