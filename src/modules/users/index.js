ae8c25d4a0b439a519801dee3a41dae33edfb431
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-24 12:34:11 +0430

    add serivce and controller functions in index.js

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -3,3 +3,5 @@
 
 export { UserModel } from './models/user.model.js';
 export { default as authRoutes } from './routes/v1/auth.routes.js';
+export { registerUser } from './services/v1/auth.service.js';
+export { register } from './controllers/v1/auth.controller.js';
