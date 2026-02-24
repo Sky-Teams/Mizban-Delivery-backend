1c9be93e37737a4ccfa61b81b62e75759aa8f04f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-24 16:18:26 +0430

    Export files. #14

--- src/modules/drivers/index.js
+++ src/modules/drivers/index.js
@@ -1,1 +1,3 @@
 export { default as driverRoutes } from './routes/v1/driver.routes.js';
+export { DriverModel } from './models/driver.model.js';
+export { doesDriverExist, createNewDriver } from './services/v1/driver.service.js';
