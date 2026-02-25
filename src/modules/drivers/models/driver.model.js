50c6e3d4f55c342a50aefeafad13e8ee896781d0
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-25 08:51:30 +0430

    Add unique index on user field. #14

--- src/modules/drivers/models/driver.model.js
+++ src/modules/drivers/models/driver.model.js
@@ -6,6 +6,7 @@ const DriverSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
+      unique: true,
     },
     vehicleType: {
       type: String,
