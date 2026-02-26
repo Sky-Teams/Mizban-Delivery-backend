ac9f7bf3b9cedd2e2e075481a31e941d90d269af
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-26 23:15:26 +0430

    use sperate function in controller

--- src/modules/users/controllers/v1/auth.controller.js
+++ src/modules/users/controllers/v1/auth.controller.js
@@ -1,7 +1,12 @@
-import { registerUser } from '../../services/v1/auth.service.js';
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+import { AppError } from '#shared/errors/error.js';
+import { doesUserExist, registerUser } from '../../services/v1/auth.service.js';
 
 export const register = async (req, res) => {
   const data = req.body;
+
+  const emailExist = await doesUserExist({ email: data.email });
+  if (emailExist) throw new AppError('Email already exists', 400, ERROR_CODES.DUPLICATE);
 
   await registerUser(data);
 
