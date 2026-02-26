5324f0fa9b93679ddf08deedd0dff1ba82c10f32
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-27 00:20:30 +0430

    add validation for register user in route

---
+++ src/modules/users/dto/register.user.schema.js
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { isValidPhoneNumber } from 'libphonenumber-js';
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+
+const registerUserSchema = z.object({
+  body: z.object({
+    name: z.string().trim().min(3, { message: ERROR_CODES.MIN_LENGTH_IS_3_CHARACTERS }),
+    email: z.string().trim().email({ message: ERROR_CODES.INVALID_EMAIL }),
+    phone: z.string().refine((val) => isValidPhoneNumber(val, 'AF'), {
+      message: ERROR_CODES.INVALID_PHONE_NUMBER,
+    }),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: ERROR_CODES.PASSWORD_MUST_BE_AT_LEAST_6_CHARACTERS }),
+  }),
+});
+
+export const registerUserValidator = (req) => {
+  return registerUserSchema.safeParse({ body: req.body });
+};
--- src/modules/users/routes/v1/auth.routes.js
+++ src/modules/users/routes/v1/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { register } from '../../controllers/v1/auth.controller.js';
+import { validate } from '#shared/middleware/validate.js';
+import { registerUserValidator } from '../../dto/register.user.schema.js';
 
 const router = express.Router();
 
-router.post('/register', asyncHandler(register));
+router.post('/register', validate(registerUserValidator), asyncHandler(register));
 
 export default router;
