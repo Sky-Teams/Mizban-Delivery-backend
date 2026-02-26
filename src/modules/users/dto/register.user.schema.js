3d12f7c24b51db343d9ea39a461ad145eb262b14
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-27 02:23:39 +0430

    add lowercase to normalize email

--- src/modules/users/dto/register.user.schema.js
+++ src/modules/users/dto/register.user.schema.js
@@ -4,7 +4,7 @@ import { ERROR_CODES } from '#shared/errors/customCodes.js';
 
 const registerUserSchema = z.object({
   body: z.object({
-    name: z.string().trim().min(3, { message: ERROR_CODES.MIN_LENGTH_IS_3_CHARACTERS }),
+    name: z.string().trim().min(3, { message: ERROR_CODES.MIN_LENGTH_IS_3_CHARACTERS }).toLowerCase(),
     email: z.string().trim().email({ message: ERROR_CODES.INVALID_EMAIL }),
     phone: z.string().refine((val) => isValidPhoneNumber(val, 'AF'), {
       message: ERROR_CODES.INVALID_PHONE_NUMBER,
