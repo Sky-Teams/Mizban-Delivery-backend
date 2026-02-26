356f33c61770315a33a83295550a397778e98890
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-27 00:22:48 +0430

    add more test for controller and services

--- src/tests/unit/modules/users/v1/auth.controller.test.js
+++ src/tests/unit/modules/users/v1/auth.controller.test.js
@@ -1,9 +1,11 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import { registerUser } from '#modules/users/index.js';
+import { registerUser, doesUserExist } from '#modules/users/index.js';
 import { register } from '#modules/users/index.js';
+import { AppError } from '#shared/errors/error.js';
 
 vi.mock('#modules/users/services/v1/auth.service.js', () => ({
   registerUser: vi.fn(),
+  doesUserExist: vi.fn(),
 }));
 
 describe('Auth controller - register', () => {
@@ -27,15 +29,42 @@ describe('Auth controller - register', () => {
     };
   });
 
+  it('should throw error if user already exists', async () => {
+    doesUserExist.mockResolvedValue(true);
+
+    await expect(register(req, res)).rejects.toThrow(AppError);
+
+    expect(doesUserExist).toHaveBeenCalledWith({ email: req.body.email });
+    expect(registerUser).not.toHaveBeenCalled();
+  });
+
   it('should call registerUser and return success response', async () => {
+    doesUserExist.mockResolvedValue(false);
+    registerUser.mockResolvedValue({ id: '123', email: 'user@gmail.com' });
+
     await register(req, res);
 
+    expect(doesUserExist).toHaveBeenCalledWith({ email: 'user@gmail.com' });
     expect(registerUser).toHaveBeenCalledWith(req.body);
-    expect(res.status).toHaveBeenCalledWith(200);
 
+    expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({
       success: true,
       message: 'User registered successfully',
     });
+  });
+
+  it('Password not returned in response', async () => {
+    doesUserExist.mockResolvedValue(false);
+    registerUser.mockResolvedValue({
+      id: '123',
+      email: 'user@gmail.com',
+      password: 'hashed_password',
+    });
+
+    await register(req, res);
+
+    const responseCall = res.json.mock.calls[0][0];
+    expect(responseCall).not.toHaveProperty('password');
   });
 });
--- src/tests/unit/modules/users/v1/auth.service.test.js
+++ src/tests/unit/modules/users/v1/auth.service.test.js
@@ -1,11 +1,13 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
 import bcrypt from 'bcryptjs';
-import { registerUser } from '#modules/users/index.js';
+import { registerUser, doesUserExist } from '#modules/users/index.js';
 import { UserModel } from '#modules/users/index.js';
-import { AppError } from '#shared/errors/error.js';
-import { ERROR_CODES } from '#shared/errors/customCodes.js';
 
-vi.mock('bcryptjs');
+vi.mock('bcryptjs', () => ({
+  default: {
+    hash: vi.fn(),
+  },
+}));
 vi.mock('#modules/users/models/user.model.js', () => ({
   UserModel: {
     exists: vi.fn(),
@@ -13,50 +15,80 @@ vi.mock('#modules/users/models/user.model.js', () => ({
   },
 }));
 
-describe('Auth Service - registerUser', () => {
+describe('Auth Service ', () => {
   const userData = {
     email: 'test@gmail.com',
     name: 'test',
     phone: '1233456576',
     password: 'password123',
   };
+  const fakeUser = {
+    _id: '12',
+    email: userData.email,
+  };
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it('should throw an error if email already exists', async () => {
-    UserModel.exists.mockResolvedValue(true);
+  describe('doesUserExist', () => {
+    it('should return true if email exists in UserModel', async () => {
+      UserModel.exists.mockResolvedValue(true);
+      const result = await doesUserExist({ email: userData.email });
 
-    await expect(registerUser(userData)).rejects.toThrow(AppError);
-    await expect(registerUser(userData)).rejects.toMatchObject({
-      message: 'Email already exists',
-      code: ERROR_CODES.DUPLICATE,
+      expect(result).toBe(true);
+    });
+
+    it('should return false if email does not exists', async () => {
+      UserModel.exists.mockResolvedValue(false);
+      const result = await doesUserExist({ email: userData.email });
+
+      expect(result).toBe(false);
     });
   });
 
-  it('should create a new user with hashed password', async () => {
-    UserModel.exists.mockResolvedValue(false);
-    bcrypt.hash.mockResolvedValue('hashedPassword');
+  describe('registerUser', () => {
+    it('bcrypt.hash is called with the correct password', async () => {
+      bcrypt.hash.mockResolvedValue('hash_password');
 
-    const fakeUser = {
-      _id: '12',
-      email: userData.email,
-    };
-    UserModel.create.mockResolvedValue(fakeUser);
+      UserModel.create.mockResolvedValue({ _id: fakeUser._id, email: fakeUser.email });
+      await registerUser(userData);
 
-    const result = await registerUser(userData);
-
-    expect(bcrypt.hash).toHaveBeenCalledWith(userData.password, 12);
-    expect(UserModel.create).toHaveBeenCalledWith({
-      name: userData.name,
-      phone: userData.phone,
-      email: userData.email,
-      password: 'hashedPassword',
+      expect(bcrypt.hash).toHaveBeenCalledWith('password123', 12);
     });
 
-    expect(result).toEqual({
-      id: fakeUser._id,
-      email: fakeUser.email,
+    it('bcrypt.hash throws an error -> AppError is thrown', async () => {
+      bcrypt.hash.mockRejectedValue(new Error('Hashing failed'));
+
+      expect(UserModel.create).not.toHaveBeenCalled();
+    });
+
+    it('should throw an error if the database fails', async () => {
+      bcrypt.hash.mockResolvedValue('hashed_password');
+
+      UserModel.create.mockRejectedValue(new Error('Database connection failed'));
+
+      await expect(registerUser(userData)).rejects.toThrow('Database connection failed');
+    });
+
+    it('should create a new user with hashed password', async () => {
+      bcrypt.hash.mockResolvedValue('hashedPassword');
+
+      UserModel.create.mockResolvedValue(fakeUser);
+
+      const result = await registerUser(userData);
+
+      expect(bcrypt.hash).toHaveBeenCalledWith(userData.password, 12);
+      expect(UserModel.create).toHaveBeenCalledWith({
+        name: userData.name,
+        phone: userData.phone,
+        email: userData.email,
+        password: 'hashedPassword',
+      });
+
+      expect(result).toEqual({
+        id: fakeUser._id,
+        email: fakeUser.email,
+      });
     });
   });
 });
