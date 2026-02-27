0307c0093ac46fcce700c2f34817b65a3e03cc2e
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-27 15:32:27 +0430

    Split login service function into many helper functions #4

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,25 +1,15 @@
-import bcrypt from 'bcryptjs';
-import { UserModel } from '../../models/user.model.js';
+import { getUserByEmail, validateLoginUser } from '#shared/utils/auth.helper.js';
 import { generateAccessToken } from '#shared/utils/jwt.js';
-import { AppError } from '#shared/errors/error.js';
-import { ERROR_CODES } from '#shared/errors/customCodes.js';
 
 export const loginService = async ({ email, password }) => {
-  const user = await UserModel.findOne({ email });
-  if (!user) throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  const user = await getUserByEmail(email);
 
-  const psMatch = await bcrypt.compare(password, user.password);
-  if (!psMatch)
-    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
-
-  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
-
-  const token = generateAccessToken(user);
+  await validateLoginUser(user, password);
 
   return {
     id: user._id,
     email: user.email,
     role: user.role,
-    token,
+    token: generateAccessToken(user),
   };
 };
