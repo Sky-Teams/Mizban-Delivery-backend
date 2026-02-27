a0c3d259ef66f516ed36f15379a0db4d81b33b53
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-27 21:18:30 +0430

    Add comments #9

--- src/modules/users/services/v1/auth.service.js
+++ src/modules/users/services/v1/auth.service.js
@@ -1,13 +1,72 @@
+import bcrypt from 'bcryptjs';
+import { UserModel } from '../../models/user.model.js';
 import { RefreshTokenModel } from '../../models/refreshToken.model.js';
 import { generateAccessToken, generateRefreshToken, hashToken } from '#shared/utils/jwt.js';
-import {
-  getActiveUser,
-  getStoredRefreshToken,
-  getUserByEmail,
-  REFRESH_TOKEN_EXPIRES_TIME,
-  rotateRefreshToken,
-  validateLoginUser,
-} from '#shared/utils/auth.helper.js';
+import { AppError, unauthorized } from '#shared/errors/error.js';
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+
+const REFRESH_TOKEN_EXPIRES_TIME = 7 * 24 * 60 * 60 * 1000;
+
+const getUserByEmail = async (email) => {
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  }
+
+  return user;
+};
+
+const validateLoginUser = async (user, password) => {
+  const psMatch = await bcrypt.compare(password, user.password);
+  if (!psMatch) {
+    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  }
+
+  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
+};
+
+const getStoredRefreshToken = async ({ refreshToken, deviceId }) => {
+  if (!refreshToken || !deviceId) {
+    throw new AppError('Unauthorized: Invalid credential', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  }
+
+  const currentToken = await RefreshTokenModel.findOne({
+    token: hashToken(refreshToken),
+    deviceId,
+  }).populate('user', '_id email');
+
+  if (!currentToken) throw new AppError('Token not found', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  if (currentToken.expireAt < new Date()) {
+    await currentToken.deleteOne();
+    throw new AppError('Token expired', 401, ERROR_CODES.INVALID_CREDENTIAL);
+  }
+
+  return currentToken;
+};
+
+const getActiveUser = async (userId) => {
+  const user = await UserModel.findById(userId);
+  if (!user) throw unauthorized();
+  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
+
+  return user;
+};
+
+const rotateRefreshToken = async (currentTokenId) => {
+  const newRefreshToken = generateRefreshToken();
+
+  await RefreshTokenModel.findOneAndUpdate(
+    { _id: currentTokenId },
+    {
+      token: hashToken(newRefreshToken),
+      expireAt: new Date(Date.now() + REFRESH_TOKEN_EXPIRES_TIME),
+    },
+    { new: true, runValidators: true, setDefaultsOnInsert: true }
+  );
+
+  return newRefreshToken;
+};
+//----------------------
 
 export const loginService = async ({ email, password }, deviceId) => {
   const user = await getUserByEmail(email);
--- src/shared/utils/auth.helper.js
+++ src/shared/utils/auth.helper.js
@@ -1,14 +1,5 @@
-import bcrypt from 'bcryptjs';
-import { UserModel } from '#modules/users/models/user.model.js';
 import { AppError } from '#shared/errors/error.js';
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
-import { RefreshTokenModel } from '#modules/users/models/refreshToken.model.js';
-import { generateRefreshToken } from '#shared/utils/jwt.js';
-
-export const REFRESH_TOKEN_EXPIRES_TIME = 7 * 24 * 60 * 60 * 1000;
-
-// --------------------
-// loginService helpers
 
 export const getDeviceId = (req) => {
   const deviceId = req.body?.deviceId;
@@ -17,67 +8,4 @@ export const getDeviceId = (req) => {
     throw new AppError('Unauthorized: Invalid credential', 401, ERROR_CODES.INVALID_CREDENTIAL);
 
   return deviceId;
-};
-
-export const getUserByEmail = async (email) => {
-  const user = await UserModel.findOne({ email });
-  if (!user) {
-    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
-  }
-
-  return user;
-};
-
-export const validateLoginUser = async (user, password) => {
-  const psMatch = await bcrypt.compare(password, user.password);
-  if (!psMatch) {
-    throw new AppError('Invalid email or password', 401, ERROR_CODES.INVALID_CREDENTIAL);
-  }
-
-  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
-};
-
-// --------------------
-// refreshService helpers
-
-export const getStoredRefreshToken = async (refreshToken, deviceId) => {
-  if (!refreshToken || !deviceId)
-    throw new AppError('Unauthorized: Invalid credential', 401, ERROR_CODES.INVALID_CREDENTIAL);
-
-  const currentToken = await RefreshTokenModel.findOne({
-    token: hashToken(refreshToken),
-    deviceId,
-  }).populate('user', '_id email');
-
-  if (!currentToken) throw new AppError('Token not found', 401, ERROR_CODES.INVALID_CREDENTIAL);
-  if (currentToken.expireAt < new Date()) {
-    await currentToken.deleteOne();
-    throw new AppError('Token expired', 401, ERROR_CODES.INVALID_CREDENTIAL);
-  }
-
-  return currentToken;
-};
-
-export const getActiveUser = async (userId) => {
-  const user = await UserModel.findById(userId);
-  if (!user) throw unauthorized();
-
-  if (!user.isActive) throw new AppError('Account is disabled!', 403, ERROR_CODES.ACCOUNT_DISABLED);
-
-  return user;
-};
-
-export const rotateRefreshToken = async (currentTokenId) => {
-  const newRefreshToken = generateRefreshToken();
-
-  await RefreshTokenModel.findOneAndUpdate(
-    { _id: currentTokenId },
-    {
-      token: hashToken(newRefreshToken),
-      expireAt: new Date(Date.now() + REFRESH_TOKEN_EXPIRES_TIME),
-    },
-    { new: true, runValidators: true, setDefaultsOnInsert: true }
-  );
-
-  return newRefreshToken;
 };
