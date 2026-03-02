a6fd4a2a4e52782e263b197aac987640b99fbdac
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-02 22:35:49 +0430

    add validation #20

--- src/modules/users/dto/create-businessCustomer.schema.js
+++ src/modules/users/dto/create-businessCustomer.schema.js
@@ -1,28 +1,54 @@
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
 import { z } from 'zod';
 import { isValidPhoneNumber } from 'libphonenumber-js';
+import { ensureNumber } from '#shared/utils/ensureNumber.js';
+import { ensureISODate } from '#shared/utils/ensureISODate.js';
 
-const createBusinessCustomerService = z.object({
+const createBusinessCustomerSchema = z.object({
   body: z.object({
     name: z
       .string({ required_filed: ERROR_CODES.REQUIRED_FIELD })
       .min(3, { message: ERROR_CODES.TOO_SHORT })
       .trim(),
+
     phone: z
       .string({ required_filed: ERROR_CODES.REQUIRED_FIELD })
       .refine((val) => isValidPhoneNumber(val, 'AF'), {
         message: ERROR_CODES.INVALID_PHONE_NUMBER,
       }),
+
     altPhone: z.string().trim().optional(),
+
     addressText: z.string().trim(),
+
     location: z.object({
       type: z.literal('point').optional(),
-      coordinates: z.preprocess((val) => {
-        if (!Array.isArray(val) || val.length !== 2) return val;
-        return val.map((v, i) =>
-          ensureNumber(v, `location.coordinates[${i}]`, ERROR_CODES.INVALID_COORDINATES)
-        );
-      }),
+
+      coordinates: z.preprocess(
+        (val) => {
+          if (!Array.isArray(val) || val.length !== 2) return val;
+          return val.map((v, i) =>
+            ensureNumber(v, `location.coordinates[${i}]`, ERROR_CODES.INVALID_COORDINATES)
+          );
+        },
+        z.array(z.number()).length(2, { message: ERROR_CODES.INVALID_COORDINATES })
+      ),
     }),
+
+    notes: z.string().optional(),
+
+    tags: z.array(),
+
+    lastOrderedAt: z.preprocess(
+      (val) =>
+        val ? ensureISODate(val, ERROR_CODES.INVALID_ISO_DATE_FORMAT, 'lastOrderedAt') : undefined,
+      z.date()
+    ),
+
+    totalOrders: z.number().optional(),
   }),
 });
+
+export const createBusinessCustomerValidator = (req) => {
+  return createBusinessCustomerSchema.safeParse({ body: req.body });
+};
--- src/modules/users/models/businessCustomer.model.js
+++ src/modules/users/models/businessCustomer.model.js
@@ -1,30 +1,25 @@
 import mongoose from 'mongoose';
 
-export const businessCustomerSchema = new mongoose.Schema(
-  {
-    business: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Business',
-      required: true,
-    },
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    altPhone: { type: String },
-    addressText: { type: String, required: true },
-    location: {
-      type: { type: String, enum: ['point'], default: 'point' },
-      coordinates: { type: Number, default: [0, 0] },
-    },
-    notes: { type: String },
-    tags: { type: [String], required: true },
-    isActive: { type: Boolean, default: true },
-    lastOrderedAt: { type: Date, default: null },
-    totalOrders: { type: Number, default: 0 },
+export const businessCustomerSchema = new mongoose.Schema({
+  business: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Business',
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  altPhone: { type: String },
+  addressText: { type: String, required: true },
+  location: {
+    type: { type: String, enum: ['point'], default: 'point' },
+    coordinates: { type: Number, default: [0, 0] },
+  },
+  notes: { type: String },
+  tags: { type: [String], required: true },
+  isActive: { type: Boolean, default: true },
+  lastOrderedAt: { type: Date, default: null },
+  totalOrders: { type: Number, default: 0 },
+});
 
 businessCustomerSchema.index({ business: 1 });
 businessCustomerSchema.index({ business: 1, phone: 1 }, { unique: true });
