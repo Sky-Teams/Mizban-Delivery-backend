850aa5ec05dd78cd3fa55ebfffca2b1e4d59f8ae
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-02 21:11:41 +0430

    add controller and service in index file

--- src/modules/businesses/index.js
+++ src/modules/businesses/index.js
@@ -1,2 +1,4 @@
 export { default as businessRoutes } from './routes/v1/business.routes.js';
 export { BusinessModel } from './models/business.model.js';
+export { createBusiness } from './controllers/v1/business.controller.js';
+export { createNewBusiness } from './services/v1/business.service.js';
