39184117472f598c17961bfea26ce6ffc29c19a5
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-02 21:10:55 +0430

    add zod validation for business schema

--- src/modules/businesses/dto/create-business.schema.js
+++ src/modules/businesses/dto/create-business.schema.js
@@ -1,12 +1,14 @@
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
 import { z } from 'zod';
 import { isValidPhoneNumber } from 'libphonenumber-js';
+import { ensureNumber } from '#shared/utils/ensureNumber.js';
 
 const businessTyep = ['restaurant', 'shop', 'pharmacy', 'warehouse', 'other'];
 
 const createBusinessSchema = z.object({
   body: z.object({
-    name: z.string().trim().min(3, { message: ERROR_CODES.MIN_LENGTH_IS_3_CHARACTERS }),
+    name: z.string().trim().min(3, { message: ERROR_CODES.NAME_TOO_SHORT }),
+
     type: z.enum(businessTyep, {
       errorMap: () => ({ message: ERROR_CODES.INVALID_BUSINESS_TYPE }),
     }),
@@ -18,18 +20,42 @@ const createBusinessSchema = z.object({
       })
       .optional(),
 
-    addressText: z.string().trim().min(3, { message: ERROR_CODES.MIN_LENGTH_IS_3_CHARACTERS }),
-    location: z.object({
-      type: z.literal('Point', {
-        errorMap: () => ({ message: ERROR_CODES.INVALID_LOCATION_TYPE }),
-      }),
-      coordinates: z.array(z.number()).length(2, { message: ERROR_CODES.INVALID_COORDINATES }),
-    }),
+    addressText: z.string().trim().min(3, { message: ERROR_CODES.LENGTH_IS_TOO_SHORT }),
+    location: z
+      .object({
+        type: z.literal('Point', {
+          message: ERROR_CODES.INVALID_LOCATION_TYPE,
+        }),
+        coordinates: z.preprocess(
+          (val) => {
+            if (!Array.isArray(val) || val.length !== 2) return val;
+            return [
+              ensureNumber(val[0], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
+              ensureNumber(val[1], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
+            ];
+          },
+          z.tuple([
+            z
+              .number()
+              .min(60, { message: ERROR_CODES.LNG_OUT_OF_RANGE }) //Longitude range for Afghanistan
+              .max(75, { message: ERROR_CODES.LNG_OUT_OF_RANGE }),
+            z
+              .number()
+              .min(29, { message: ERROR_CODES.LAT_OUT_OF_RANGE }) // Latitude range for Afghanistan
+              .max(39, { message: ERROR_CODES.LAT_OUT_OF_RANGE }),
+          ])
+        ),
+      })
+      .optional(),
 
     prepTimeAvgMinutes: z
-      .number()
-      .int({ message: ERROR_CODES.PREP_TIME_CANNOT_BE_INTEGARE })
-      .nonnegative({ message: ERROR_CODES.PREP_TIME_CANNOT_BE_NEGATIVE })
+      .preprocess(
+        (val) => ensureNumber(val, 'prepTimeAvgMinutes', ERROR_CODES.PREP_TIME_MUST_BE_INTEGARE),
+        z
+          .number()
+          .int({ message: ERROR_CODES.PREP_TIME_MUST_BE_INTEGARE })
+          .positive({ message: ERROR_CODES.PREP_TIME_MUST_BE_POSITIVE })
+      )
       .optional(),
   }),
 });
