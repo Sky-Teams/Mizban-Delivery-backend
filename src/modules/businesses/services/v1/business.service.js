3b6b5cedba3233c94b373ff449e7ff6767ced600
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-04 01:59:29 +0430

    destructure the businessData

--- src/modules/businesses/services/v1/business.service.js
+++ src/modules/businesses/services/v1/business.service.js
@@ -1,9 +1,28 @@
 import { BusinessModel } from '../../models/business.model.js';
 
+//Create new Business
 export const createNewBusiness = async (userId, businessData) => {
+  const {
+    name,
+    type: businessType,
+    phone,
+    addressText,
+    prepTimeAvgMinutes,
+    location,
+  } = businessData;
+
+  const locationData = location?.coordinates
+    ? { type: 'Point', coordinates: location.coordinates }
+    : undefined;
+
   const newBusiness = await BusinessModel.create({
-    ...businessData,
-    ownerId: userId,
+    name,
+    type: businessType,
+    phone,
+    addressText,
+    prepTimeAvgMinutes,
+    ...(locationData ? { location: locationData } : {}),
+    owner: userId,
   });
 
   return newBusiness;
