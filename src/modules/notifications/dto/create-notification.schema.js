af66cd0a94e9f627401991235a388469045de6ab
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-03 13:50:48 +0430

    Update validation schema. #28

--- src/modules/notifications/dto/create-notification.schema.js
+++ src/modules/notifications/dto/create-notification.schema.js
@@ -9,8 +9,9 @@ export const createNotificationSchema = z.object({
     message: ERROR_CODES.INVALID_USER_ID,
   }),
   type: z
-    .enum(notificationTypes, {
-      errorMap: () => ({ message: ERROR_CODES.INVALID_NOTIFICATION_TYPE }),
+    .string()
+    .refine((val) => notificationTypes.includes(val), {
+      message: ERROR_CODES.INVALID_NOTIFICATION_TYPE,
     })
     .optional(),
   title: z
@@ -21,8 +22,3 @@ export const createNotificationSchema = z.object({
     .max(100, { message: ERROR_CODES.TITLE_TOO_LONG }),
   message: z.string().max(500, { message: ERROR_CODES.MESSAGE_TOO_LONG }).optional(),
 });
-
-// We can use it in future if needed, so for now we dont need it to use as a validator in notification routes.
-// export const createNotificationValidator = (req) => {
-//   return createNotificationSchema.safeParse({ body: req.body }); // Change the body to another thing
-// };
