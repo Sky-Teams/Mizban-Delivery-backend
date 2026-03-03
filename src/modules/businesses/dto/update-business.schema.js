d7e56a9a0fb26f9e972a2dec857c82f45e9bd05c
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-04 01:03:54 +0430

    add schema validation

--- src/modules/businesses/dto/update-business.schema.js
+++ src/modules/businesses/dto/update-business.schema.js
@@ -3,14 +3,14 @@ import { z } from 'zod';
 import { isValidPhoneNumber } from 'libphonenumber-js';
 import { ensureNumber } from '#shared/utils/ensureNumber.js';
 
-const businessTyep = ['restaurant', 'shop', 'pharmacy', 'warehouse', 'other'];
+const businessType = ['restaurant', 'shop', 'pharmacy', 'warehouse', 'other'];
 
 const updateBusinessSchema = z.object({
   body: z
     .object({
       name: z.string().trim().min(3, { message: ERROR_CODES.NAME_TOO_SHORT }),
 
-      type: z.enum(businessTyep, {
+      type: z.enum(businessType, {
         errorMap: () => ({ message: ERROR_CODES.INVALID_BUSINESS_TYPE }),
       }),
 
@@ -20,28 +20,28 @@ const updateBusinessSchema = z.object({
       addressText: z.string().trim().min(3, { message: ERROR_CODES.LENGTH_IS_TOO_SHORT }),
       location: z
         .object({
-          type: z.literal('Point', {
-            message: ERROR_CODES.INVALID_LOCATION_TYPE,
-          }),
-          coordinates: z.preprocess(
-            (val) => {
-              if (!Array.isArray(val) || val.length !== 2) return val;
-              return [
-                ensureNumber(val[0], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
-                ensureNumber(val[1], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
-              ];
-            },
-            z.tuple([
-              z
-                .number()
-                .min(60, { message: ERROR_CODES.LNG_OUT_OF_RANGE }) //Longitude range for Afghanistan
-                .max(75, { message: ERROR_CODES.LNG_OUT_OF_RANGE }),
-              z
-                .number()
-                .min(29, { message: ERROR_CODES.LAT_OUT_OF_RANGE }) // Latitude range for Afghanistan
-                .max(39, { message: ERROR_CODES.LAT_OUT_OF_RANGE }),
-            ])
-          ),
+          type: z.literal('Point').optional(),
+          coordinates: z
+            .preprocess(
+              (val) => {
+                if (!Array.isArray(val) || val.length !== 2) return val;
+                return [
+                  ensureNumber(val[0], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
+                  ensureNumber(val[1], 'location.coordinates', ERROR_CODES.INVALID_COORDINATES),
+                ];
+              },
+              z.tuple([
+                z
+                  .number()
+                  .min(60, { message: ERROR_CODES.LNG_OUT_OF_RANGE }) //Longitude range for Afghanistan
+                  .max(75, { message: ERROR_CODES.LNG_OUT_OF_RANGE }),
+                z
+                  .number()
+                  .min(29, { message: ERROR_CODES.LAT_OUT_OF_RANGE }) // Latitude range for Afghanistan
+                  .max(39, { message: ERROR_CODES.LAT_OUT_OF_RANGE }),
+              ])
+            )
+            .optional(),
         })
         .partial(),
 
