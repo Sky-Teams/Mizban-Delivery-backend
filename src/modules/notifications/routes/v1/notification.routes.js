98aad7e4dd5d20d7d015ab6c194d0d4f17a6615a
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-03 13:51:47 +0430

    Add Id validator middleware. #28

--- src/modules/notifications/routes/v1/notification.routes.js
+++ src/modules/notifications/routes/v1/notification.routes.js
@@ -5,11 +5,17 @@ import {
   markNotificationAsRead,
   markNotificationAsUnread,
 } from '../../controllers/v1/notification.controller.js';
+import { validate } from '#shared/middleware/validate.js';
+import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
 
 const router = express.Router();
 
 router.get('/', asyncHandler(getUserNotifications));
-router.patch('/:id/markAsRead', asyncHandler(markNotificationAsRead));
-router.patch('/:id/markAsUnread', asyncHandler(markNotificationAsUnread));
+router.patch('/:id/markAsRead', validate(mongoIdValidator), asyncHandler(markNotificationAsRead));
+router.patch(
+  '/:id/markAsUnread',
+  validate(mongoIdValidator),
+  asyncHandler(markNotificationAsUnread)
+);
 
 export default router;
