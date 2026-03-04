3cfa817accce96d3dd07bec96c92bb93754585b6
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-04 13:02:50 +0430

    validate Id

--- src/modules/businesses/routes/v1/business.routes.js
+++ src/modules/businesses/routes/v1/business.routes.js
@@ -1,11 +1,17 @@
 import { updateBusinessValidator } from '#modules/businesses/dto/update-business.schema.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
+import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
 import { validate } from '#shared/middleware/validate.js';
 import { updateBusiness } from '../../controllers/v1/business.controller.js';
 import express from 'express';
 
 const router = express.Router();
 
-router.put('/:id', validate(updateBusinessValidator), asyncHandler(updateBusiness));
+router.put(
+  '/:id',
+  validate(mongoIdValidator),
+  validate(updateBusinessValidator),
+  asyncHandler(updateBusiness)
+);
 
 export default router;
