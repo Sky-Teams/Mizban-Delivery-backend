43d73f06d170952a90b519e0686243f1c147884b
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-04 16:56:27 +0430

    Create route #21

--- src/modules/businessCustomers/routes/v1/businessCustomer.routes.js
+++ src/modules/businessCustomers/routes/v1/businessCustomer.routes.js
@@ -1,5 +1,9 @@
-import { createBusinessCustomer } from '#modules/businessCustomers/controllers/v1/businessCustomer.controller.js';
+import {
+  createBusinessCustomer,
+  updateBusinessCustomer,
+} from '#modules/businessCustomers/controllers/v1/businessCustomer.controller.js';
 import { createBusinessCustomerValidator } from '#modules/businessCustomers/dto/create.businessCustomer.schema.js';
+import { updateBusinessCustomerValidator } from '#modules/businessCustomers/dto/update.businessCustomer.schema.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import express from 'express';
@@ -9,5 +13,9 @@ const router = express.Router();
 router
   .route('/')
   .post(validate(createBusinessCustomerValidator), asyncHandler(createBusinessCustomer));
+
+router
+  .route('/:id')
+  .patch(validate(updateBusinessCustomerValidator), asyncHandler(updateBusinessCustomer));
 
 export default router;
