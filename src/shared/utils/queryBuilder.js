0932449db9880bda9d6483741bd9defc4a8d67a1
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 14:05:33 +0430

    Add helper functions. #31

--- src/shared/utils/queryBuilder.js
+++ src/shared/utils/queryBuilder.js
@@ -21,3 +21,68 @@ export const driverQueryBuilder = (searchQuery) => {
 
   return query;
 };
+
+// Used only for admin services
+export const filterDriverField = (driverData) => {
+  const driverAllowedFields = {
+    vehicleType: true,
+    status: true,
+    capacity: true,
+    currentLocation: true,
+    lastLocationAt: true,
+    vehicleRegistrationNumber: true,
+    address: true,
+    timeAvailability: true,
+    isVerified: true,
+  };
+
+  const driverUpdateQuery = {};
+
+  for (const key of Object.keys(driverAllowedFields)) {
+    if (key in driverData && !['capacity', 'currentLocation', 'timeAvailability'].includes(key)) {
+      driverUpdateQuery[key] = driverData[key];
+    }
+  }
+
+  // Handle nested object for capacity
+  if (driverData.capacity) {
+    if (driverData.capacity.maxWeightKg !== undefined) {
+      driverUpdateQuery['capacity.maxWeightKg'] = driverData.capacity.maxWeightKg;
+    }
+
+    if (driverData.capacity.maxPackages !== undefined) {
+      driverUpdateQuery['capacity.maxPackages'] = driverData.capacity.maxPackages;
+    }
+  }
+
+  // Handle currentLocation coordinates
+  if (driverData.currentLocation?.coordinates !== undefined) {
+    driverUpdateQuery['currentLocation.coordinates'] = driverData.currentLocation.coordinates;
+  }
+
+  if (driverData.timeAvailability) {
+    if (driverData.timeAvailability.start !== undefined)
+      driverUpdateQuery['timeAvailability.start'] = driverData.timeAvailability.start;
+    if (driverData.timeAvailability.end !== undefined)
+      driverUpdateQuery['timeAvailability.end'] = driverData.timeAvailability.end;
+  }
+
+  return driverUpdateQuery;
+};
+
+export const filterUserField = async (data) => {
+  const userAllowedFields = {
+    name: true,
+    email: true,
+    phone: true,
+    password: true,
+  };
+
+  const userUpdateQuery = {};
+  for (const key of Object.keys(userAllowedFields)) {
+    if (key in data) {
+      userUpdateQuery[key] = key === 'password' ? await hashPassword(data[key]) : data[key];
+    }
+  }
+  return userUpdateQuery;
+};
