336729e53040321a5e7b46e6f38be98827bed272
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 11:06:07 +0430

    Implement hashPassword function. #31

--- src/shared/utils/jwt.js
+++ src/shared/utils/jwt.js
@@ -1,5 +1,10 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 export const verifyJWT = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+export const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 12);
 };
