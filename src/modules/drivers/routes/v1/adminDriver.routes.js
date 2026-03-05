a72ca7bebb9e097eb102e5bfd6180e73e5a5ddc8
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 11:10:15 +0430

    Add createDriver route for admin. #31

--- src/modules/drivers/routes/v1/adminDriver.routes.js
+++ src/modules/drivers/routes/v1/adminDriver.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllDrivers, getDriver } from '../../controllers/v1/driver.controller.js';
+import { addDrvier, getAllDrivers, getDriver } from '../../controllers/v1/driver.controller.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
+import { adminCreateDriverValidator } from '../../dto/admin-create-driver.schema.js';
 
 const router = express.Router();
 
+router.post('/', validate(adminCreateDriverValidator), asyncHandler(addDrvier));
 router.get('/', asyncHandler(getAllDrivers));
-
 router.get('/:id', validate(mongoIdValidator), asyncHandler(getDriver)); // route /:id => id is driverId
 
 export default router;
