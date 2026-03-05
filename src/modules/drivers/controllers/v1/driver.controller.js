52e32e7e041a0c1410fac579e908d0d178ba9a6b
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 11:08:08 +0430

    Implement addDrvier controller. #31

--- src/modules/drivers/controllers/v1/driver.controller.js
+++ src/modules/drivers/controllers/v1/driver.controller.js
@@ -1,6 +1,8 @@
+import { doesUserExist } from '#modules/users/index.js';
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
 import { AppError, notFound, unauthorized } from '#shared/errors/error.js';
 import {
+  addNewDriver,
   createNewDriver,
   doesDriverExist,
   fetchDriverByDriverId,
@@ -9,35 +11,20 @@ import {
   updateExistedDriver,
 } from '../../services/v1/driver.service.js';
 
-export const createDriver = async (req, res) => {
+//#region Admin controllers
+
+export const addDrvier = async (req, res) => {
   if (!req.user) throw unauthorized();
 
-  const exist = await doesDriverExist(req.user._id);
-  if (exist) throw new AppError('Driver already exist', 400, ERROR_CODES.DRIVER_ALREADY_EXIST);
+  const doesEmailExist = await doesUserExist({ email: req.body.email });
+  if (doesEmailExist) throw new AppError('Email already exist', 400, ERROR_CODES.DUPLICATE);
 
-  const driver = await createNewDriver(req.user._id, req.body);
-  res.status(201).json({ success: true, data: driver });
+  const newDriver = await addNewDriver(req.body);
+
+  res.status(200).json({ success: true, data: newDriver });
 };
 
-export const updateDriver = async (req, res) => {
-  if (!req.user) throw unauthorized();
-
-  const updatedDriver = await updateExistedDriver(req.params.id, req.user._id, req.body);
-  res.status(200).json({ success: true, data: updatedDriver });
-};
-
-// Return driver info by userId
-export const getDriverProfile = async (req, res) => {
-  if (!req.user) throw unauthorized();
-
-  const driverInfo = await getDriverInfoByUserId(req.user._id);
-  if (!driverInfo) throw notFound('Driver');
-
-  res.status(200).json({ success: true, data: driverInfo });
-};
-
-// Admin controllers
-
+// Fetch All Drivers
 export const getAllDrivers = async (req, res) => {
   if (!req.user) throw unauthorized();
 
@@ -69,3 +56,38 @@ export const getDriver = async (req, res) => {
 
   res.status(200).json({ success: true, data: driver });
 };
+
+//#endregion
+
+//#region User Routes => We will need these controllers in future. For now we dont need them
+
+// User can request to create a driver account
+export const createDriver = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const exist = await doesDriverExist(req.user._id);
+  if (exist) throw new AppError('Driver already exist', 400, ERROR_CODES.DRIVER_ALREADY_EXIST);
+
+  const driver = await createNewDriver(req.user._id, req.body);
+  res.status(201).json({ success: true, data: driver });
+};
+
+// User can update its driver account
+export const updateDriver = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const updatedDriver = await updateExistedDriver(req.params.id, req.user._id, req.body);
+  res.status(200).json({ success: true, data: updatedDriver });
+};
+
+// Return driver info by userId
+export const getDriverProfile = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const driverInfo = await getDriverInfoByUserId(req.user._id);
+  if (!driverInfo) throw notFound('Driver');
+
+  res.status(200).json({ success: true, data: driverInfo });
+};
+
+//#endregion
