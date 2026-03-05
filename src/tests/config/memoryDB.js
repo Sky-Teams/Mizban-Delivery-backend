183f9f62201fd36a9d93dfcd516e8542d076db1b
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 17:12:19 +0430

    Update config. #31

--- src/tests/config/memoryDB.js
+++ src/tests/config/memoryDB.js
@@ -1,35 +1,46 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import jwt from 'jsonwebtoken';
 import { UserModel } from '#modules/users/index.js';
 
-let mongoServer;
+let replSet;
 
 export const connectDB = async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
+  replSet = await MongoMemoryReplSet.create({
+    replSet: {
+      count: 1,
+      storageEngine: 'wiredTiger', // required for transactions
+    },
+  });
+
+  const uri = replSet.getUri();
   await mongoose.connect(uri);
 };
 
 export const disconnectDB = async () => {
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
-  await mongoServer.stop();
+
+  if (replSet) {
+    await replSet.stop();
+  }
 };
 
 export const clearDB = async () => {
   const collections = mongoose.connection.collections;
+
   for (const key in collections) {
     await collections[key].deleteMany();
   }
 };
 
-export const createFakeUserWithToken = async () => {
+export const createFakeUserWithToken = async (role = 'customer') => {
   const user = await UserModel.create({
     name: 'Test User',
     email: 'test@example.com',
     password: 'hashedpassword123',
+    role,
   });
 
   const secret = process.env.JWT_SECRET || 'mizban-delivery-system-key';
