e66981279ca70bf0785fb8a1a1f6deb55861f56c
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-05 17:13:36 +0430

    add update routes. #31

--- src/modules/drivers/routes/v1/adminDriver.routes.js
+++ src/modules/drivers/routes/v1/adminDriver.routes.js
@@ -9,12 +9,18 @@ import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
 import { adminCreateDriverValidator } from '../../dto/admin-create-driver.schema.js';
+import { adminUpdateDriverValidator } from '../../dto/admin-update-driver.schema.js';
 
 const router = express.Router();
 
 router.post('/', validate(adminCreateDriverValidator), asyncHandler(addDrvier));
 router.get('/', asyncHandler(getAllDrivers));
 router.get('/:id', validate(mongoIdValidator), asyncHandler(getDriver)); // route /:id => id is driverId
-router.put('/:id', validate(mongoIdValidator), asyncHandler(modifyDriver)); // route /:id => id is driverId
+router.put(
+  '/:id',
+  validate(mongoIdValidator),
+  validate(adminUpdateDriverValidator),
+  asyncHandler(modifyDriver)
+); // route /:id => id is driverId
 
 export default router;
