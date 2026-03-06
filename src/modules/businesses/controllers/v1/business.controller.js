6da6557fd9a39f0fc2299b5f8cd6fac2dfa8bb78
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-06 15:10:03 +0430

    Create controller #39

--- src/modules/businesses/controllers/v1/business.controller.js
+++ src/modules/businesses/controllers/v1/business.controller.js
@@ -1,12 +1,47 @@
-import { unauthorized } from '#shared/errors/error.js';
-import { createNewBusiness } from '../../services/v1/business.service.js';
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+import { AppError, notFound, unauthorized } from '#shared/errors/error.js';
+import {
+  createNewBusiness,
+  DoesBusinessesExist,
+  getAllBusinesses,
+  getBusinessById,
+} from '../../services/v1/business.service.js';
 
 //Create new Business
 export const createBusiness = async (req, res) => {
   if (!req.user) throw unauthorized();
 
+  const exist = await DoesBusinessesExist(req.user._id, req.body.phone);
+  if (exist) throw new AppError('Business already exist', 400, ERROR_CODES.BUSINESS_ALREADY_EXIST);
+
   const business = await createNewBusiness(req.user._id, req.body);
   res.status(201).json({
+    success: true,
+    data: business,
+  });
+};
+
+// Get All Businesses
+export const getBusinesses = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const allBusinesses = await getAllBusinesses();
+
+  res.status(200).json({
+    success: true,
+    data: allBusinesses,
+  });
+};
+
+// Get Business By ID
+export const getBusiness = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const business = await getBusinessById(req.params.id);
+
+  if (!business) throw notFound('Business');
+
+  res.status(200).json({
     success: true,
     data: business,
   });
