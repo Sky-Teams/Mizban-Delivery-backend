45a564d6a2fc05b87943198005ae8de289fa051c
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-06 15:09:16 +0430

    Create service #39

--- src/modules/businesses/services/v1/business.service.js
+++ src/modules/businesses/services/v1/business.service.js
@@ -29,3 +29,15 @@ export const createNewBusiness = async (userId, businessData) => {
 
   return newBusiness;
 };
+
+export const getAllBusinesses = async () => {
+  const allBusinesses = await BusinessModel.find().populate('owner', 'name email').lean();
+
+  return allBusinesses;
+};
+
+export const getBusinessById = async (businessId) => {
+  const business = await BusinessModel.findById(businessId).populate('owner', 'name email');
+
+  return business;
+};
