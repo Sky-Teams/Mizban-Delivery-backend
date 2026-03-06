55a687153df745c0f167c73a6213ece5389108a0
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-06 21:40:15 +0430

    add admin routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import { corsOptions } from './config/cors.js';
 import { errorHandler } from './shared/middleware/errorHandler.js';
-import { businessRoutes } from '#modules/businesses/index.js';
+import { adminBusinessRoutes, businessRoutes } from '#modules/businesses/index.js';
 import { authRoutes } from '#modules/users/index.js';
 import { authMiddleware } from '#shared/middleware/authMiddleware.js';
 import { driverRoutes } from '#modules/drivers/index.js';
 import { businessRoutes } from '#modules/businesses/index.js';
 import { notificationRoutes } from '#modules/notifications/index.js';
+import { authorizeRole } from '#shared/middleware/authorizeRole.js';
 
 const app = express();
 
@@ -35,6 +36,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/drivers', authMiddleware, driverRoutes);
 app.use('/api/notifications', authMiddleware, notificationRoutes);
 app.use('/api/businesses', authMiddleware, businessRoutes);
+
+//Admin routes
+app.use('/api/admin/businesses', authMiddleware, adminBusinessRoutes);
 
 //#endregion
 
