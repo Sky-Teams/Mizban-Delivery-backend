03d401523d7b0c4d2b25b8370c92ac5de5cb90cb
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-06 21:53:19 +0430

    fix type

--- src/modules/businesses/routes/v1/adminBusiness.routes.js
+++ src/modules/businesses/routes/v1/adminBusiness.routes.js
@@ -3,7 +3,7 @@ import { createBusiness, updateBusiness } from '../../controllers/v1/business.co
 import { createBusinessValidator } from '../../dto/create-business.schema.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
-import { updateBusinessValidator } from '#modules/businesses/dto/update-business.schema.js';
+import { updateBusinessValidator } from '../../dto/update-business.schema.js';
 import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
 
 const router = express.Router();
