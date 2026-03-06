5d94320853d42f56017b9e27aae703bd0e6c69e2
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-06 15:10:35 +0430

    re-export  route and create custom code #39

--- src/modules/businesses/index.js
+++ src/modules/businesses/index.js
@@ -1,4 +1,5 @@
 export { default as businessRoutes } from './routes/v1/business.routes.js';
+export { default as adminBusinessRoutes } from './routes/v1/admin-business.routes.js';
 export { BusinessModel } from './models/business.model.js';
 export { createBusiness } from './controllers/v1/business.controller.js';
 export { createNewBusiness } from './services/v1/business.service.js';
