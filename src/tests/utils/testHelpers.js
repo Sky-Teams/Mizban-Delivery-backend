fb8300b9b0be306845f9356c471fba1f786c9a16
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-08 14:36:39 +0430

    Add asyn to function. #41

--- src/tests/utils/testHelpers.js
+++ src/tests/utils/testHelpers.js
@@ -1,6 +1,6 @@
 import request from 'supertest';
 
 // helper function to send authenticated POST requests in integration tests
-export const postWithAuth = (app, url, data, token) => {
-  return request(app).post(url).send(data).set('Authorization', `Bearer ${token}`);
+export const postWithAuth = async (app, url, data, token) => {
+  return await request(app).post(url).send(data).set('Authorization', `Bearer ${token}`);
 };
