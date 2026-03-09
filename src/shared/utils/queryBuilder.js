483c7cc6a6135fb190b5275fceb9c648f878f68c
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-09 22:46:07 +0430

    Implement deliveryRequestUpdateQuery function. #46

--- src/shared/utils/queryBuilder.js
+++ src/shared/utils/queryBuilder.js
@@ -1,3 +1,5 @@
+import { calculateItemsTotal } from './math.helper.js';
+
 export const driverQueryBuilder = (searchQuery) => {
   const query = {};
   if (searchQuery.vehicleType) query.vehicleType = searchQuery.vehicleType;
@@ -85,4 +87,70 @@ export const filterUserField = async (data) => {
     }
   }
   return userUpdateQuery;
+};
+
+export const deliveryRequestUpdateQuery = (deliveryRequestData, allowedFields) => {
+  const updateQuery = {};
+
+  Object.keys(deliveryRequestData).forEach((key) => {
+    if (!allowedFields[key]) return;
+
+    const value = deliveryRequestData[key];
+
+    switch (key) {
+      case 'sender':
+        if (!value) break;
+
+        if (value.id !== undefined) updateQuery['sender.id'] = value.id;
+        if (value.name !== undefined) updateQuery['sender.name'] = value.name;
+        if (value.phone !== undefined) updateQuery['sender.phone'] = value.phone;
+        break;
+
+      case 'receiver':
+        if (!value) break;
+
+        if (value.id !== undefined) updateQuery['receiver.id'] = value.id;
+        if (value.name !== undefined) updateQuery['receiver.name'] = value.name;
+        if (value.phone !== undefined) updateQuery['receiver.phone'] = value.phone;
+        if (value.address !== undefined) updateQuery['receiver.address'] = value.address;
+        break;
+
+      case 'packageDetails':
+        if (!value) break;
+
+        if (value.weight !== undefined) updateQuery['packageDetails.weight'] = value.weight;
+        if (value.size !== undefined) updateQuery['packageDetails.size'] = value.size;
+        if (value.fragile !== undefined) updateQuery['packageDetails.fragile'] = value.fragile;
+        if (value.note !== undefined) updateQuery['packageDetails.note'] = value.note;
+        break;
+
+      case 'deliveryPrice':
+        if (!value) break;
+        if (value.total !== undefined) updateQuery['deliveryPrice.total'] = value.total;
+        break;
+
+      case 'items':
+        if (Array.isArray(value)) {
+          updateQuery.items = calculateItemsTotal(value);
+        }
+        break;
+
+      case 'pickupLocation':
+        if (value?.coordinates !== undefined) {
+          updateQuery['pickupLocation.coordinates'] = value.coordinates;
+        }
+        break;
+
+      case 'dropoffLocation':
+        if (value?.coordinates !== undefined) {
+          updateQuery['dropoffLocation.coordinates'] = value.coordinates;
+        }
+        break;
+
+      default:
+        updateQuery[key] = value;
+    }
+  });
+
+  return updateQuery;
 };
