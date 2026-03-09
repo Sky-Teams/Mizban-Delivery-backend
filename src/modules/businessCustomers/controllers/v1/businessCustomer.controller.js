d2c54962cdef0da58fcaaef13802867a2931633e
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-09 23:35:05 +0430

    Fix: update business-customer logic #21

--- src/modules/businessCustomers/controllers/v1/businessCustomer.controller.js
+++ src/modules/businessCustomers/controllers/v1/businessCustomer.controller.js
@@ -1,10 +1,12 @@
+import { businessCustomerModel } from '#modules/businessCustomers/models/businessCustomer.model.js';
 import {
   createNewBusinessCustomer,
   doesBusinessCustomerExist,
   updateExistedBusinessCustomer,
 } from '#modules/businessCustomers/services/v1/businessCustomer.service.js';
+import { BusinessModel } from '#modules/businesses/index.js';
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
-import { AppError, unauthorized } from '#shared/errors/error.js';
+import { AppError, notFound, unauthorized } from '#shared/errors/error.js';
 
 export const createBusinessCustomer = async (req, res) => {
   if (!req.user) throw unauthorized();
@@ -33,9 +35,17 @@ export const createBusinessCustomer = async (req, res) => {
 export const updateBusinessCustomer = async (req, res) => {
   if (!req.user) throw unauthorized();
 
+  const customer = await businessCustomerModel.findById(req.params.id);
+  if (!customer) throw notFound('Customer');
+
+  const business = await BusinessModel.findById(customer.business);
+  if (!business) throw notFound('Business');
+
+  if (!business.owner.equals(req.user._id)) throw unauthorized();
+
   const updatedBusinessCustomer = await updateExistedBusinessCustomer(
     req.params.id,
-    req.user._id,
+    business._id,
     req.body
   );
 
