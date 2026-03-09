d25d616ee21be8baddcec279d6ea9952a533cf43
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-09 14:57:59 +0430

    Fix import of file. #41

--- src/modules/deliveryRequests/routes/v1/adminDeliveryRequest.routes.js
+++ src/modules/deliveryRequests/routes/v1/adminDeliveryRequest.routes.js
@@ -2,7 +2,7 @@ import { createDelivery } from '../../controllers/v1/deliveryRequest.controller.
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import express from 'express';
 import { validate } from '#shared/middleware/validate.js';
-import { adminCreateDeliveryRequestValidator } from '#modules/deliveryRequests/dto/admin-create-delivery-request.schema.js';
+import { adminCreateDeliveryRequestValidator } from '../../dto/admin-create-delivery-request.schema.js';
 
 const router = express.Router();
 
