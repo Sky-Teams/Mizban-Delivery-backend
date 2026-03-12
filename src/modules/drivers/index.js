539cc5a94c4d130ba9b37aacf59f48598520569d
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-12 15:03:15 +0430

    Remove unneccessary exports. #51

--- src/modules/drivers/index.js
+++ src/modules/drivers/index.js
@@ -1,10 +1,8 @@
 export { default as driverRoutes } from './routes/v1/driver.routes.js';
-export { default as adminDriverRoutes } from './routes/v1/adminDriver.routes.js';
 export { DriverModel } from './models/driver.model.js';
 export {
   doesDriverExist,
   createNewDriver,
-  updateExistedDriver,
   getDriverInfoByUserId,
   modifyExistedDriver,
   addNewDriver,
@@ -12,9 +10,6 @@ export {
   fetchDrivers,
 } from './services/v1/driver.service.js';
 export {
-  createDriver,
-  updateDriver,
-  getDriverProfile,
   addDriver,
   getAllDrivers,
   getDriver,
