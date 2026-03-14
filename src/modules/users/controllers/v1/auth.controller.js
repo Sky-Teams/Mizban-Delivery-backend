f6ce6267e65dc55c9f2df27842a0345e7783ab3a
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-14 09:34:51 +0430

    Create controller function for forgot password and resetPassword #44

--- src/modules/users/controllers/v1/auth.controller.js
+++ src/modules/users/controllers/v1/auth.controller.js
@@ -1,9 +1,12 @@
-import { cookieOptions } from '#shared/utils/jwt.js';
+import { cookieOptions, hashToken } from '#shared/utils/jwt.js';
 import { ensureDeviceId, getDeviceId } from '#shared/utils/auth.helper.js';
 import { ERROR_CODES } from '#shared/errors/customCodes.js';
-import { AppError } from '#shared/errors/error.js';
-import { loginService, refreshService } from '../../services/v1/auth.service.js';
+import { AppError, notFound } from '#shared/errors/error.js';
+import { getUserByEmail, loginService, refreshService } from '../../services/v1/auth.service.js';
 import { doesUserExist, registerUser } from '../../services/v1/auth.service.js';
+import { UserModel } from '#modules/users/models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { agenda } from '../../../../config/agenda.js';
 
 export const register = async (req, res) => {
   const data = req.body;
@@ -46,5 +49,59 @@ export const refreshAccessToken = async (req, res) => {
   res.status(200).json({
     success: true,
     data: { token: accessToken },
+  });
+};
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await getUserByEmail(email);
+  if (!user) throw notFound('User');
+
+  const resetToken = user.createPasswordResetToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetUrl = `http://localhost:3000/api/auth/reset-password/${resetToken}`;
+
+  await agenda.now('send-reset-password-email', {
+    email: user.email,
+    username: user.name,
+    resetUrl,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Email sent',
+  });
+};
+
+export const resetPassword = async (req, res) => {
+  const { resetToken } = req.params;
+  const { newPassword, confirmPassword } = req.body;
+
+  const user = await UserModel.findOne({
+    passwordResetToken: hashToken(resetToken),
+    passwordResetExpires: { $gt: new Date() },
+  });
+
+  if (!user) throw new AppError('Invalid or expired token', 400, ERROR_CODES.INVALID_TOKEN);
+
+  if (newPassword !== confirmPassword)
+    throw new AppError('Password not match', 400, ERROR_CODES.PASSWORD_NOT_MATCHING);
+
+  const newPasswordHashed = await bcrypt.hash(newPassword, 12);
+
+  await user
+    .set({
+      password: newPasswordHashed,
+      passwordResetToken: null,
+      passwordResetExpires: null,
+    })
+    .save();
+
+  res.status(200).json({
+    success: true,
+    message: 'Password updated successfully',
   });
 };
