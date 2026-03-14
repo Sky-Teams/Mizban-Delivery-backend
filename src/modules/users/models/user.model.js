018a0f51b6c72f452e8b112bb849945bccae8ced
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-14 09:35:49 +0430

    Add create password resetToken method on schema #44

--- src/modules/users/models/user.model.js
+++ src/modules/users/models/user.model.js
@@ -1,42 +1,18 @@
 import mongoose from 'mongoose';
+import crypto from 'crypto';
 
 const UserSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-    },
-    phone: {
-      type: String,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    googleId: {
-      type: String,
-      default: null,
-    },
-    role: {
-      type: String,
-      enum: ['customer', 'driver', 'business', 'admin'],
-      default: 'customer',
-    },
-
-    changedPasswordAt: {
-      type: Date,
-      default: null,
-    },
-
+    name: { type: String, required: true, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    phone: { type: String, trim: true },
+    password: { type: String, required: true },
+    googleId: { type: String, default: null },
+    role: { type: String, enum: ['customer', 'driver', 'business', 'admin'], default: 'customer' },
+    changedPasswordAt: { type: Date, default: null },
     isActive: { type: Boolean, default: true },
+    passwordResetToken: { type: String, default: null },
+    passwordResetExpires: { type: Date, default: null },
   },
   {
     timestamps: true,
@@ -52,5 +28,15 @@ UserSchema.index(
     },
   }
 );
+
+UserSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  this.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
+
+  return resetToken;
+};
 
 export const UserModel = mongoose.model('User', UserSchema);
