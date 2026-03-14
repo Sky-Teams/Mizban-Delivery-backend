40e71a57b0b8c9f08f94258c623bbb32713ebb04
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-14 12:43:35 +0430

    Create route #44

--- src/modules/users/routes/v1/auth.routes.js
+++ src/modules/users/routes/v1/auth.routes.js
@@ -10,13 +10,19 @@ import { validate } from '#shared/middleware/validate.js';
 import { loginValidator } from '../../dto/login.schema.js';
 import { register } from '../../controllers/v1/auth.controller.js';
 import { registerUserValidator } from '../../dto/register.user.schema.js';
+import { forgotPasswordValidator } from '#modules/users/dto/forgot-password.schema.js';
+import { resetPasswordValidator } from '../../dto/reset-password.schema.js';
 
 const router = express.Router();
 
 router.post('/register', validate(registerUserValidator), asyncHandler(register));
 router.post('/login', validate(loginValidator), asyncHandler(login));
 router.post('/refresh', asyncHandler(refreshAccessToken));
-router.post('/forgot-password', asyncHandler(forgotPassword));
-router.post('/reset-password/:resetToken', asyncHandler(resetPassword));
+router.post('/forgot-password', validate(forgotPasswordValidator), asyncHandler(forgotPassword));
+router.post(
+  '/reset-password/:resetToken',
+  validate(resetPasswordValidator),
+  asyncHandler(resetPassword)
+);
 
 export default router;
