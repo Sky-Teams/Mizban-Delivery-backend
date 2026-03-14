9ad243594599af58d3614e9f5c0c63b352e1eaaa
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-14 09:38:18 +0430

    Create routes #44

--- src/modules/users/routes/v1/auth.routes.js
+++ src/modules/users/routes/v1/auth.routes.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { login, refreshAccessToken } from '../../controllers/v1/auth.controller.js';
+import {
+  forgotPassword,
+  login,
+  refreshAccessToken,
+  resetPassword,
+} from '../../controllers/v1/auth.controller.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import { loginValidator } from '../../dto/login.schema.js';
@@ -11,5 +16,7 @@ const router = express.Router();
 router.post('/register', validate(registerUserValidator), asyncHandler(register));
 router.post('/login', validate(loginValidator), asyncHandler(login));
 router.post('/refresh', asyncHandler(refreshAccessToken));
+router.post('/forgot-password', asyncHandler(forgotPassword));
+router.post('/reset-password/:resetToken', asyncHandler(resetPassword));
 
 export default router;
