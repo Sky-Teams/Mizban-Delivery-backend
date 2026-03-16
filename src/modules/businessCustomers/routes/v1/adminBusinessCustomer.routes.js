e2dc91d95fcbb99fb52aaf39cecf4eeee3804061
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-16 22:49:41 +0430

    use query validators

--- src/modules/businessCustomers/routes/v1/adminBusinessCustomer.routes.js
+++ src/modules/businessCustomers/routes/v1/adminBusinessCustomer.routes.js
@@ -4,6 +4,7 @@ import {
 } from '#modules/businessCustomers/controllers/v1/businessCustomer.controller.js';
 import { createBusinessCustomerValidator } from '#modules/businessCustomers/dto/create.businessCustomer.schema.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
+import { queriesValidator } from '#shared/middleware/queryValidator.js';
 import { validate } from '#shared/middleware/validate.js';
 import express from 'express';
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router
   .route('/')
   .post(validate(createBusinessCustomerValidator), asyncHandler(createBusinessCustomer))
-  .get(asyncHandler(getBusinessCustomers));
+  .get(validate(queriesValidator), asyncHandler(getBusinessCustomers));
+
 export default router;
