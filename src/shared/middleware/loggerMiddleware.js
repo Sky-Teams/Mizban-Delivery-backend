d0bb71f20fd70984ea6f073825d6e6a8ff81cf0c
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-26 19:32:08 +0430

    logger middleware

--- src/shared/middleware/loggerMiddleware.js
+++ src/shared/middleware/loggerMiddleware.js
@@ -1,7 +1,57 @@
-import { logger } from '#shared/utils/logger.js';
+import { logger } from '#shared/logger/logger.js';
+import { randomUUID } from 'crypto';
 
-export const logMiddleware = (req, res, next) => {
-  if (req.originalUrl.startsWith('/api/logs')) {
-    return next();
-  }
+export const loggerMiddleware = (req, res, next) => {
+  const auditMethods = { POST: 'Create', PUT: 'Update', DELETE: 'Delete' };
+  const startDate = new Date();
+  let isSuccess;
+
+  res.on('finish', () => {
+    const isAuditAction = !!auditMethods[req.method];
+    const userId = req.user?._id
+    isSuccess = res.statusCode >= 200 && res.statusCode < 300;
+    
+    const logData = {
+      logId: randomUUID(),
+      userId: req.user?._id, 
+      method: req.method,
+      path: req.originalUrl,
+      statusCode: res.statusCode,
+      duration: `${new Date() - startDate}ms`,
+      clientIp:
+        req.ip === '::1'
+          ? '127.0.0.1'
+          : req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      userAgent: req.headers['user-agent'],
+    };
+
+    if (isAuditAction && userId) {
+      logger.info(
+        {
+          ...logData,
+          logType: 'AUDIT',
+          userRole: req.user?.role,
+          action: auditMethods[req.method],
+          payload: req.body,
+          success: isSuccess,
+        },
+        `Audit: ${auditMethods[req.method]} operation`
+      );
+    } else {
+      logger.info({ ...logData, logType: 'ACCESS' }, 'Request completed');
+    }
+
+    if (res.statusCode >= 500) {
+      logger.error(
+        {
+          ...logData,
+          logType: 'ERROR',
+          errorData: res.locals.errorData || null,
+        },
+        'System error detected'
+      );
+    }
+  });
+
+  next();
 };
