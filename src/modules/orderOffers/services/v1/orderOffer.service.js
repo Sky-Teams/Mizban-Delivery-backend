2c8735ce45a5ec7499571a4818de680a48f6eccc
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-03-30 22:48:31 +0430

    Implement createOrderOffer function. #66

---
+++ src/modules/orderOffers/services/v1/orderOffer.service.js
@@ -0,0 +1,27 @@
+import { OrderOfferModel } from '#modules/orderOffers/models/orderOffer.model.js';
+import { createOderOfferSchema } from '../../dto/create-order-offer.schema.js';
+
+/**
+ * Create a new offer for an order in the system.
+ * This function is intended to be used internally by other services,
+ * not via an API route, so validation is performed here in the service layer.
+    @param {String} orderId 
+    @param {String} driverId
+    @returns {Object}  orderOffer object
+ */
+export const createOrderOffer = async (orderId, driverId) => {
+  const orderOffer = { order: orderId, driver: driverId };
+  createOderOfferSchema.parse(orderOffer);
+
+  const orderOfferObject = {
+    order: orderId,
+    driver: driverId,
+    offeredAt: Date.now(), //TODO: Check the UTC format
+  };
+
+  const newOrderOffer = await OrderOfferModel.create(orderOfferObject);
+
+  return newOrderOffer;
+};
+
+// export const updateOrderOffer = async(orderId)
