2a07ecb163c1659d2e25603c418786a63f40fa0a
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-30 22:40:07 +0430

    Add error message #59

--- src/shared/errors/errorMessages.js
+++ src/shared/errors/errorMessages.js
@@ -66,6 +66,12 @@ export const ERROR_MESSAGES = {
     ps: 'پټنوم بدل شوی دی',
   },
 
+  [ERROR_CODES.PASSWORDS_NOT_MATCHES]: {
+    en: 'Passwords do not match',
+    fa: 'رمزهای عبور مطابقت ندارند',
+    ps: 'پټنومونه سره سمون نلري',
+  },
+
   [ERROR_CODES.NO_FIELDS_PROVIDED]: {
     en: 'No fields provided for update',
     fa: ' هیچ فیلدی برای به‌روزرسانی ارائه نشده است.',
