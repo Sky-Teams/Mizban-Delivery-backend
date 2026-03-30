0671824dcc2313e1554856b6cd498a887d87b9c7
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-03-30 17:03:34 +0430

    Add routes for change password and logout #59

--- src/modules/users/routes/v1/auth.routes.js
+++ src/modules/users/routes/v1/auth.routes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { login, refreshAccessToken } from '../../controllers/v1/auth.controller.js';
+import { authMiddleware } from '#shared/middleware/authMiddleware.js';
+import {
+  changePassword,
+  login,
+  logout,
+  refreshAccessToken,
+} from '../../controllers/v1/auth.controller.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import { loginValidator } from '../../dto/login.schema.js';
+import { changePasswordValidator } from '../../dto/change-password.schema.js';
 import { register } from '../../controllers/v1/auth.controller.js';
 import { registerUserValidator } from '../../dto/register.user.schema.js';
 
@@ -11,5 +18,12 @@ const router = express.Router();
 router.post('/register', validate(registerUserValidator), asyncHandler(register));
 router.post('/login', validate(loginValidator), asyncHandler(login));
 router.post('/refresh', asyncHandler(refreshAccessToken));
+router.post(
+  '/change-password',
+  authMiddleware,
+  validate(changePasswordValidator),
+  asyncHandler(changePassword)
+);
+router.post('/logout', authMiddleware, asyncHandler(logout));
 
 export default router;
