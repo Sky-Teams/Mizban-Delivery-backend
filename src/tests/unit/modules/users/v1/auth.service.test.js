9067114d6b0edd14219282a97a011a54eadd8290
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-31 06:09:24 -0700

    fix  test

--- src/tests/unit/modules/users/v1/auth.service.test.js
+++ src/tests/unit/modules/users/v1/auth.service.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, vi, expectTypeOf } from 'vitest';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 import bcrypt from 'bcryptjs';
 import {
   registerUser,
