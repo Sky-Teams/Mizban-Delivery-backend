aec010c999ffba50e92509e945664a3965e37693
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-03-31 06:00:46 -0700

    Implement route for googleOauth

--- src/modules/users/routes/v1/auth.routes.js
+++ src/modules/users/routes/v1/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, refreshAccessToken } from '../../controllers/v1/auth.controller.js';
+import { googleLogin, login, refreshAccessToken } from '../../controllers/v1/auth.controller.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { validate } from '#shared/middleware/validate.js';
 import { loginValidator } from '../../dto/login.schema.js';
@@ -12,4 +12,5 @@ router.post('/register', validate(registerUserValidator), asyncHandler(register)
 router.post('/login', validate(loginValidator), asyncHandler(login));
 router.post('/refresh', asyncHandler(refreshAccessToken));
 
+router.post('/google', asyncHandler(googleLogin));
 export default router;
