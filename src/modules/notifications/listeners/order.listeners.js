663a1c4a33a8a2abc2312ab8114483b63ba85ea7
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-01 17:04:30 +0430

    Update order listener. #66

--- src/modules/notifications/listeners/order.listeners.js
+++ src/modules/notifications/listeners/order.listeners.js
@@ -8,9 +8,11 @@ import { createNotificationForAdmins } from '../services/v1/notification.service
 export const registerOrderListeners = () => {
   eventBus.on('order:created', async (data) => {
     const orderId = data.orderId.toString();
+    //TODO We should create a centralized notification service for sending notification
     const payload = NotificationPayloads.orderCreated(orderId);
     CustomSocket.emitToAdmins('notification', payload);
     await createNotificationForAdmins(payload.type, payload.title, payload.message);
+
     const drivers = await findNearestAndScore(data.newOrder.pickupLocation.coordinates);
 
     // TODO we must search what should we do if no driver found. for now we just send an notification for all admins
