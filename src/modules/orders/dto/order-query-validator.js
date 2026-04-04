d5d73a888c4115898d342caf0809f1269b8bde52
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-04-03 19:37:27 -0700

    create dto for query validator

--- src/modules/orders/dto/order-query-validator.js
+++ src/modules/orders/dto/order-query-validator.js
@@ -14,22 +14,6 @@ const queryValidator = z.object({
       .int({ message: ERROR_CODES.LIMIT_PARAMETER_MUST_BE_INTEGER })
       .positive({ message: ERROR_CODES.LIMIT_PARAMETER_MUST_BE_POSITIVE })
       .optional(),
-    isActive: z
-      .preprocess(
-        (val) => (val === 'false' ? false : val === 'true' ? false : val),
-        z.boolean({ message: ERROR_CODES.INVALID_BOOLEAN_TYPE })
-      )
-      .optional(),
-    sort: z
-      .enum(['top', 'latest'], {
-        message: ERROR_CODES.INVALID_SORT_OPTION,
-      })
-      .optional(),
-    businessId: z
-      .string()
-      .refine((val) => mongoose.Types.ObjectId.isValid(val), { message: ERROR_CODES.INVALID_ID })
-      .optional(),
-
     status: z
       .enum(['created', 'assigned', 'pickedUp', 'delivered', 'cancelled'], {
         message: ERROR_CODES.INVALID_STATUS,
@@ -52,6 +36,6 @@ const queryValidator = z.object({
   }),
 });
 
-export const queriesValidator = (req) => {
+export const orderQueryValidator = (req) => {
   return queryValidator.safeParse({ query: req.query });
 };
