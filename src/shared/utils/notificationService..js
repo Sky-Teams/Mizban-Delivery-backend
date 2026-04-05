a5cffb5096aeacec12012344dc201fe4fa289e2a
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-05 19:31:07 +0430

    Implement NotificationService. #78

---
+++ src/shared/utils/notificationService..js
@@ -0,0 +1,40 @@
+import { CustomSocket } from '#config/socket.js';
+import { createNotificationForAdmins } from '#modules/notifications/index.js';
+
+export class NotificationService {
+  /**
+   * Central notification sender
+   * @param {'admins'|'driver'|'user'} to
+   * @param {string} event
+   * @param {Object} payload
+   * @param {string|null} userId
+   * @param {boolean} persist
+   */
+  static async send(to, event, payload, userId = null, persist = true) {
+    switch (to) {
+      case 'admins':
+        CustomSocket.emitToAdmins(event, payload);
+
+        if (persist) {
+          await createNotificationForAdmins(payload.type, payload.title, payload.message);
+        }
+        break;
+
+      case 'driver':
+      case 'user':
+        if (!userId) {
+          throw new Error('userId is required for driver/user notifications');
+        }
+
+        CustomSocket.emitToUser(userId, event, payload);
+
+        if (persist) {
+          // we can store notification in db in here.
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown notification target: ${to}`);
+    }
+  }
+}
