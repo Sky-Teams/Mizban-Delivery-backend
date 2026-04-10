f7ecacb253ffd2587a56a0b2af2f4c0a55c4044a
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-10 14:19:53 +0430

    Update controller logic with transaction. #78

--- src/modules/offers/controllers/v1/offer.controller.js
+++ src/modules/offers/controllers/v1/offer.controller.js
@@ -1,14 +1,22 @@
-import { acceptAnOffer, rejectAnOffer } from '../../services/v1/offer.service.js';
+import {
+  acceptAnOfferWithTransaction,
+  rejectAnOfferWithTransaction,
+} from '../../services/v1/offer.service.js';
 import { unauthorized } from '#shared/errors/error.js';
+import { OfferService } from '#shared/utils/offerService.js';
 
 export const acceptOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
-  const offer = await acceptAnOffer(req.params.id, req.user._id);
+  const offer = await acceptAnOfferWithTransaction(req.params.id, req.user._id);
   res.status(200).json({ success: true, data: offer });
 };
 
 export const rejectOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
-  const offer = await rejectAnOffer(req.params.id, req.user._id);
+
+  const offer = await rejectAnOfferWithTransaction(req.params.id, req.user._id);
+  // sent offer to next driver, we can not move and use below logic in rejectAnOfferWithTransaction, because sendOfferToDriver should not be tight with
+  // session and transaction.
+  await OfferService.sendOfferToDriver(offer.order.toString());
   res.json({ success: true, offer });
 };
