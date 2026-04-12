ff4ed1af8ffd4c951af4dc082fc2529957a1c9b1
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-12 15:27:05 +0430

    Add job for acceptanceRate calculation. #78

--- src/modules/offers/controllers/v1/offer.controller.js
+++ src/modules/offers/controllers/v1/offer.controller.js
@@ -4,10 +4,12 @@ import {
 } from '../../services/v1/offer.service.js';
 import { unauthorized } from '#shared/errors/error.js';
 import { OfferService } from '#shared/utils/offerService.js';
+import { agenda } from '#config/agenda.js';
 
 export const acceptOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
   const offer = await acceptAnOfferWithTransaction(req.params.id, req.user._id);
+  await agenda.schedule('5s', 'calculate-acceptance-rate', { userId: req.user._id });
   res.status(200).json({ success: true, data: offer });
 };
 
@@ -18,5 +20,6 @@ export const rejectOffer = async (req, res) => {
   // sent offer to next driver, we can not move and use below logic in rejectAnOfferWithTransaction, because sendOfferToDriver should not be tight with
   // session and transaction.
   await OfferService.sendOfferToDriver(offer.order.toString());
+  await agenda.schedule('5s', 'calculate-acceptance-rate', { userId: req.user._id });
   res.json({ success: true, offer });
 };
