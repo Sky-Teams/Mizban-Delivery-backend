7dfd0695927760dae0c33588b1612c32a21d706b
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-12 15:42:50 +0430

    Add validation. #78

--- src/modules/offers/routes/v1/offer.routes.js
+++ src/modules/offers/routes/v1/offer.routes.js
@@ -1,11 +1,23 @@
 import { acceptOffer, rejectOffer } from '#modules/offers/controllers/v1/offer.controller.js';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { authorizeRole } from '#shared/middleware/authorizeRole.js';
+import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
+import { validate } from '#shared/middleware/validate.js';
 import express from 'express';
 const router = express.Router();
 
 //TODO: Add validation for :id
-router.post('/:id/accept', authorizeRole('driver'), asyncHandler(acceptOffer));
-router.post('/:id/reject', authorizeRole('driver'), asyncHandler(rejectOffer));
+router.patch(
+  '/:id/accept',
+  validate(mongoIdValidator),
+  authorizeRole('driver'),
+  asyncHandler(acceptOffer)
+);
+router.patch(
+  '/:id/reject',
+  validate(mongoIdValidator),
+  authorizeRole('driver'),
+  asyncHandler(rejectOffer)
+);
 
 export default router;
