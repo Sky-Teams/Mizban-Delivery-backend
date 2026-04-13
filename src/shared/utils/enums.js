aee2c0e0a3bcd7422de2a20735cf83af108dcd28
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-13 16:41:17 +0430

    Implement enums. #78

---
+++ src/shared/utils/enums.js
@@ -0,0 +1,20 @@
+export const OFFER_STATUS = {
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  ACCEPTED: 'accepted',
+};
+
+export const ORDER_STATUS = {
+  CREATED: 'created',
+  ASSIGNED: 'assigned',
+  PICKEDUP: 'pickedUp',
+  DELIVERED: 'delivered',
+  CANCELLED: 'cancelled',
+};
+
+export const DRIVER_STATUS = {
+  OFFLINE: 'offline',
+  IDLE: 'idle',
+  ASSIGNED: 'assigned',
+  DELIVERING: 'delivering',
+};
