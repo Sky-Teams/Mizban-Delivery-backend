d5926044db74e7775056ffa4a5f07fa2196292b3
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-13 16:40:24 +0430

    Refactor. #78

--- src/shared/utils/offerService.js
+++ src/shared/utils/offerService.js
@@ -1,14 +1,13 @@
-import { agenda } from '#config/agenda.js';
 import { fetchDriverByDriverId } from '#modules/drivers/index.js';
 import { createOffer, getOffer } from '#modules/offers/index.js';
 import { getOrderById, increaseDriverIndex } from '#modules/orders/index.js';
+import { DbJobService } from './dbJob.service.js';
 import { DtoService } from './dtoService.js';
+import { DRIVER_STATUS, OFFER_STATUS, ORDER_STATUS } from './enums.js';
 import { NotificationPayloads } from './notificationPayloadBuilder.js';
 import { NotificationService } from './notificationService.js';
 
 export class OfferService {
-  static #offerExpiresIn = process.env.Offer_Expires_In_Seconds || 30;
-
   /**
    * Send offer to a specific driver
    * @param {string} orderId
@@ -19,7 +18,7 @@ export class OfferService {
 
       // Check if order is still available
       // ex: Driver accept the offer in last seconds and timeout job is executed.
-      if (order.status !== 'created') return; // It means order is not in created mode, so we cancel the process
+      if (order.status !== ORDER_STATUS.CREATED) return; // It means order is not in created mode, so we cancel the process
 
       const driversInfo = order.recommendedDrivers;
       const currentIndex = order.currentDriverIndex;
@@ -31,6 +30,7 @@ export class OfferService {
         return;
       }
 
+      // We need this log for testing purpose
       console.log('Send offer to driver: ', driversInfo[currentIndex].id);
 
       //TODO: Does we need to store the info about the order details in offer table?
@@ -41,29 +41,29 @@ export class OfferService {
         return await OfferService.sendOfferToDriver(orderId);
       }
 
-      //TODO: Does we need to check if driver is still available? Yes because maybe driver accept another offer during this offer.
       const driver = await fetchDriverByDriverId(driversInfo[currentIndex].id);
-      if (!driver || driver.status !== 'idle' || driver.activeOrders >= driver.maxOrders) {
+      if (
+        !driver ||
+        driver.status !== DRIVER_STATUS.IDLE ||
+        driver.activeOrders >= driver.maxOrders
+      ) {
         await increaseDriverIndex(orderId);
         return await OfferService.sendOfferToDriver(orderId);
       }
 
+      // Create Offer payload for the driver to understand the details of the offer
       const orderInfo = DtoService.order(order);
-      orderInfo.expiresIn = this.#offerExpiresIn;
+      orderInfo.expiresIn = process.env.Offer_Expires_In_Seconds || 30;
       orderInfo.eta = driversInfo[currentIndex].eta;
       orderInfo.distance = driversInfo[currentIndex].distance;
-
       const offerPayload = NotificationPayloads.orderOffered(orderInfo);
+
+      // Send offer to driver
       await NotificationService.send('driver', 'offer', offerPayload, driver.user._id);
 
       console.log('Offer Id: ', offer._id); // we need it for test
 
-      // Schedule timeout job. For simulating the process, timeout is set to 30s
-      await agenda.schedule(`${this.#offerExpiresIn}s`, 'offer:timeout', {
-        orderId,
-        driverId: offer.driver,
-        offerId: offer._id,
-      });
+      await DbJobService.scheduleOfferTimeout(orderId, offer.driver, offer._id);
     } catch (error) {
       // For validation Error, we send offer to next driver without blocking the recommendation service.
       if (error.name === 'ZodError') {
@@ -90,7 +90,7 @@ export class OfferService {
    * @param {Array} param0.drivers
    */
   static async handleOfferTimeout({ orderId, driverId }) {
-    console.log('Timeout');
+    console.log('Timeout. No Response From Driver'); // We need this log for testing purpose
 
     const offer = await getOffer(orderId, driverId);
     if (!offer) {
@@ -99,21 +99,21 @@ export class OfferService {
     }
 
     switch (offer.status) {
-      case 'pending':
+      case OFFER_STATUS.PENDING:
         //TODO: In future we need to take care of race condition if we have more than one process.
         offer.status = 'expired';
         await offer.save();
         await increaseDriverIndex(orderId);
         await OfferService.sendOfferToDriver(orderId);
+        const driver = await fetchDriverByDriverId(driverId);
+        if (!driver) return;
+        await DbJobService.scheduleCalculateAcceptanceRate(driver.user._id);
         break;
 
-      //TODO: In here, if the order is rejected, so it means, the rejectOffer function already sent the offer to next driver
-      case 'rejected':
-        // await OfferService.sendOfferToDriver(orderId, drivers, driverIndex + 1);
-        break;
-
-      case 'accepted':
-        console.log(`Offer already accepted by driver ${driverId}`);
+      // In here, if the order is rejected, so it means, the rejectOffer function in offer service layer already sent the offer to next driver
+      case OFFER_STATUS.REJECTED:
+      case OFFER_STATUS.ACCEPTED:
+        console.log(`Offer already accepted/rejected by driver ${driverId}`);
         break;
     }
   }
