33f4ff11eba152c786a6e1905d15a08c6b1374a7
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-13 16:06:32 +0430

    Use DbJobService. #78

--- src/modules/offers/controllers/v1/offer.controller.js
+++ src/modules/offers/controllers/v1/offer.controller.js
@@ -4,12 +4,12 @@ import {
 } from '../../services/v1/offer.service.js';
 import { unauthorized } from '#shared/errors/error.js';
 import { OfferService } from '#shared/utils/offerService.js';
-import { agenda } from '#config/agenda.js';
+import { DbJobService } from '#shared/utils/dbJob.service.js';
 
 export const acceptOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
   const offer = await acceptAnOfferWithTransaction(req.params.id, req.user._id);
-  await agenda.schedule('5s', 'calculate-acceptance-rate', { userId: req.user._id });
+  await DbJobService.scheduleCalculateAcceptanceRate(req.user._id);
   res.status(200).json({ success: true, data: offer });
 };
 
@@ -17,9 +17,7 @@ export const rejectOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
 
   const offer = await rejectAnOfferWithTransaction(req.params.id, req.user._id);
-  // sent offer to next driver, we can not move and use below logic in rejectAnOfferWithTransaction, because sendOfferToDriver should not be tight with
-  // session and transaction.
   await OfferService.sendOfferToDriver(offer.order.toString());
-  await agenda.schedule('5s', 'calculate-acceptance-rate', { userId: req.user._id });
+  await DbJobService.scheduleCalculateAcceptanceRate(req.user._id);
   res.json({ success: true, offer });
 };
