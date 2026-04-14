f20744609af4c672f9ccf61265ccf80c891c9b4b
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-14 16:56:29 +0430

    Use custom event constant. #90

--- src/modules/notifications/listeners/order.listeners.js
+++ src/modules/notifications/listeners/order.listeners.js
@@ -1,12 +1,13 @@
 import { findNearestAndScore } from '#modules/drivers/index.js';
 import { addDriversDataInOrder } from '#modules/orders/index.js';
 import { eventBus } from '#shared/event-bus/eventBus.js';
+import { CUSTOM_EVENTS } from '#shared/utils/enums.js';
 import { NotificationPayloads } from '#shared/utils/notificationPayloadBuilder.js';
 import { NotificationService } from '#shared/utils/notificationService.js';
 import { OfferService } from '#shared/utils/offerService.js';
 
 export const registerOrderListeners = () => {
-  eventBus.on('order:created', async (data) => {
+  eventBus.on(CUSTOM_EVENTS.ORDER_CREATED, async (data) => {
     const orderId = data.orderId.toString();
 
     const payload = NotificationPayloads.orderCreated(orderId);
