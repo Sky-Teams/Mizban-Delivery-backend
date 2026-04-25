ebe6de4e79af8040be0c1ac20ec2400a68db8d8f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-25 15:55:08 +0430

    Add orderPickedUp payload builder. #90

--- src/shared/utils/notificationPayloadBuilder.js
+++ src/shared/utils/notificationPayloadBuilder.js
@@ -1,31 +1,39 @@
+import { NOTIFICATION_TYPE } from './enums.js';
+
 // All message is created in here and can be used in all listeners
 export const NotificationPayloads = {
   orderCreated: (orderId) => ({
-    type: 'ORDER',
+    type: NOTIFICATION_TYPE.ORDER,
     title: 'New Order',
     message: `Order ${orderId} has been created`,
   }),
 
   orderAssigned: (orderId) => ({
-    type: 'ORDER',
+    type: NOTIFICATION_TYPE.ORDER,
     title: 'Order Assigned',
     message: `Order ${orderId} has been assigned`,
   }),
 
   orderOffered: (metadata) => ({
-    type: 'OFFER',
+    type: NOTIFICATION_TYPE.OFFER,
     title: 'New Offer',
     message: 'An offer is ready to pickup',
     metadata,
   }),
   noDriverFound: (orderId) => ({
-    type: 'NO-DRIVER',
+    type: NOTIFICATION_TYPE.NO_DRIVER,
     title: 'No Driver Found',
     message: `No Driver found for order ${orderId}`,
   }),
   systemError: (message) => ({
-    type: 'SYSTEM',
+    type: NOTIFICATION_TYPE.SYSTEM,
     title: 'System Error',
     message,
+  }),
+
+  orderPickedUp: (orderId, driverId, pickedUpAt) => ({
+    type: NOTIFICATION_TYPE.ORDER,
+    title: 'Order pickded Up',
+    message: `Order (${orderId}) is pickdedUp by driver (${driverId}) at ${pickedUpAt}`,
   }),
 };
