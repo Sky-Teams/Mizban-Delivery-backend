4f594708e1b6ab4148f44499efaf8fa2796de40f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-04-26 17:49:27 +0430

    Add new payloads for notification. #90

--- src/shared/utils/notificationPayloadBuilder.js
+++ src/shared/utils/notificationPayloadBuilder.js
@@ -36,4 +36,28 @@ export const NotificationPayloads = {
     title: 'Order pickded Up',
     message: `Order (${orderId}) is pickdedUp by driver (${driverId}) at ${pickedUpAt}`,
   }),
+
+  orderDelivered: (orderId, driverId, deliveredAt) => ({
+    type: NOTIFICATION_TYPE.ORDER,
+    title: 'Order delivered',
+    message: `Order (${orderId}) is deliverd by driver (${driverId}) at ${deliveredAt}`,
+  }),
+
+  orderCancelled: (orderId, cancelReason) => ({
+    type: NOTIFICATION_TYPE.ORDER,
+    title: 'Order cancelled',
+    message: `Order (${orderId}) is cancelled. Reason ${cancelReason}`,
+  }),
+
+  offerAccepted: (orderId, driverId) => ({
+    type: NOTIFICATION_TYPE.OFFER,
+    title: 'Offer Accepted',
+    message: `Offer for Order (${orderId}) is accepted by driver (${driverId})`,
+  }),
+
+  offerRejected: (orderId, driverId) => ({
+    type: NOTIFICATION_TYPE.OFFER,
+    title: 'Offer Rejected',
+    message: `Offer for Order (${orderId}) is rejected by driver (${driverId})`,
+  }),
 };
