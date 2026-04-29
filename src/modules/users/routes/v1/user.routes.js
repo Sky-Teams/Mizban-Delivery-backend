9643e8851fb193af997e11b7e33ed23f2258c6f1
fataneh-mow <fataneh.mow@gmail.com>
2026-04-29 18:28:12 +0430

    fix: created the saveFcmToken to save the token sent by front and added a new route to users route for that #79

--- src/modules/users/routes/v1/user.routes.js
+++ src/modules/users/routes/v1/user.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { asyncHandler } from '#shared/middleware/asyncHandler.js';
 import { getProfile } from '../../controllers/v1/user.contoller.js';
+import { saveFcmToken } from '../../controllers/v1/user.contoller.js';
+import { authMiddleware } from '#shared/middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', asyncHandler(getProfile));
+router.post('/save-fcm-token', authMiddleware, asyncHandler(saveFcmToken)); // created new route for posting fcmToken
 
 export default router;
