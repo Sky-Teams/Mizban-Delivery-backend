39834618d22e89d5296f050b4e5a7599ad7b5476
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-05-06 10:30:06 +0430

    Export functions. #101

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -14,5 +14,6 @@ export {
 export { register, googleLogin } from './controllers/v1/auth.controller.js';
 
 export { default as userRoutes } from './routes/v1/user.routes.js';
-export { getProfile } from './controllers/v1/user.contoller.js';
+export { default as deviceRoutes } from './routes/v1/device.routes.js';
+export { getProfile, registerDevice, updateDevice } from './controllers/v1/user.controller.js';
 export { getUserProfile } from './services/v1/user.service.js';
