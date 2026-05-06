1105967b46c8ddcec65dc7d19d3dca1c63b600a7
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-05-06 10:32:45 +0430

    Implement addDevice and updateDeviceInfo functions. #101

--- src/modules/users/services/v1/user.service.js
+++ src/modules/users/services/v1/user.service.js
@@ -12,3 +12,58 @@ export const getUserProfile = async (userId) => {
     phone: profile?.phone,
   };
 };
+
+export const addDevice = async (userId, fcmToken, platform, deviceId) => {
+  const updatedUser = await UserModel.findOneAndUpdate(
+    {
+      _id: userId,
+      'devices.deviceId': deviceId,
+    },
+    {
+      $set: {
+        'devices.$.fcmToken': fcmToken,
+        'devices.$.platform': platform,
+      },
+    },
+    { new: true }
+  );
+
+  if (updatedUser) return updatedUser;
+
+  return await UserModel.findByIdAndUpdate(
+    userId,
+    {
+      $push: {
+        devices: { platform, fcmToken, deviceId },
+      },
+    },
+    { new: true }
+  );
+};
+
+export const updateDeviceInfo = async (userId, fcmToken, platform, deviceId) => {
+  const updateFields = {};
+
+  if (fcmToken) {
+    updateFields['devices.$.fcmToken'] = fcmToken;
+  }
+
+  if (platform) {
+    updateFields['devices.$.platform'] = platform;
+  }
+
+  const updatedUser = await UserModel.findOneAndUpdate(
+    {
+      _id: userId,
+      'devices.deviceId': deviceId,
+    },
+    {
+      $set: updateFields,
+    },
+    { new: true }
+  );
+
+  if (!updatedUser) throw notFound('Device');
+
+  return updatedUser;
+};
