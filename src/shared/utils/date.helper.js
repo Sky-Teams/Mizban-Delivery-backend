31f2bde06f1a5914eab14fe0a0cc10cf750003eb
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-05-19 09:54:39 +0430

    Implement date of birth validation. #108

--- src/shared/utils/date.helper.js
+++ src/shared/utils/date.helper.js
@@ -49,4 +49,27 @@ export class DateHelper {
 
     return dateOnly.test(val) || isoDate.test(val);
   };
+
+  /**
+   * Validate Date of birth
+   * @param {string} val
+   * @returns
+   */
+  static isValidBirthDate(val) {
+    if (typeof val !== 'string') return false;
+
+    const match = val.match(/^(\d{4})\/(\d{2})\/(\d{2})$/);
+    if (!match) return false;
+
+    const year = Number(match[1]);
+    const month = Number(match[2]);
+    const day = Number(match[3]);
+
+    const date = new Date(year, month - 1, day);
+
+    //TODO: We can add age range validation for a driver. Ex: Driver must be more than 18.
+
+    // Check if the entered date is valid or not (ex: 2026/10/32 => invalid day(32))
+    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
+  }
 }
