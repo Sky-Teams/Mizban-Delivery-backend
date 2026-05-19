c4854f701ca71601fea435b4ea6ca632c1a1555c
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-05-19 09:47:05 +0430

    Add registration route for driver profile. #108

--- src/modules/drivers/routes/v1/driver.routes.js
+++ src/modules/drivers/routes/v1/driver.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   addDriver,
+  createDriver,
   getAllDrivers,
   getDriver,
   modifyDriver,
@@ -10,14 +11,37 @@ import { validate } from '#shared/middleware/validate.js';
 import { mongoIdValidator } from '#shared/middleware/mongoIdValidator.js';
 import { createDriverValidator } from '../../dto/create-driver.schema.js';
 import { updateDriverValidator } from '../../dto/update-driver.schema.js';
+import { authorizeRole } from '#shared/middleware/authorizeRole.js';
+import { ROLES } from '#shared/utils/enums.js';
+import { createDriverProfileValidator } from '#modules/drivers/dto/user-create-driver.schema.js';
+import uploadFile from '#shared/middleware/uploadFile.js';
 
 const router = express.Router();
 
-router.post('/', validate(createDriverValidator), asyncHandler(addDriver));
-router.get('/', asyncHandler(getAllDrivers));
-router.get('/:id', validate(mongoIdValidator), asyncHandler(getDriver)); // route /:id => id is driverId
+router.post(
+  '/',
+  authorizeRole(ROLES.ADMIN),
+  validate(createDriverValidator),
+  asyncHandler(addDriver)
+);
+router.post(
+  '/registration',
+  authorizeRole(ROLES.DRIVER),
+  uploadFile.fields([
+    { name: 'photo', maxCount: 1 },
+    { name: 'nationalIdCardFront', maxCount: 1 },
+    { name: 'nationalIdCardBack', maxCount: 1 },
+    { name: 'driverLicense', maxCount: 1 },
+    { name: 'vehicleCard', maxCount: 1 },
+  ]),
+  validate(createDriverProfileValidator),
+  asyncHandler(createDriver)
+);
+router.get('/', authorizeRole(ROLES.ADMIN), asyncHandler(getAllDrivers));
+router.get('/:id', authorizeRole(ROLES.ADMIN), validate(mongoIdValidator), asyncHandler(getDriver)); // route /:id => id is driverId
 router.put(
   '/:id', // route /:id => id is driverId
+  authorizeRole(ROLES.ADMIN),
   validate(mongoIdValidator),
   validate(updateDriverValidator),
   asyncHandler(modifyDriver)
