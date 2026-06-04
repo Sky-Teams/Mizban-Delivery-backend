e35bf895b9baf9cd8d690c7b87fc41cf7e0fe875
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-06-04 09:42:18 +0430

    SCRUM-19 Export removeFCMToken function from user module

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -17,4 +17,4 @@ export { register, googleLogin } from './controllers/v1/auth.controller.js';
 export { default as userRoutes } from './routes/v1/user.routes.js';
 export { default as deviceRoutes } from './routes/v1/device.routes.js';
 export { getProfile, registerDevice, updateDevice } from './controllers/v1/user.controller.js';
-export { getUserProfile } from './services/v1/user.service.js';
+export { getUserProfile, removeFCMToken } from './services/v1/user.service.js';
