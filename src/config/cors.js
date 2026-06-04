5733ce187c1325bb8f3cc60d5b77c77e8bcb3daf
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-06-04 08:25:52 +0430

    SCRUM-24 Add local and production frontent URLs in cors origin

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,4 @@
-const origins = process.env.FRONTEND_URL;
+const origins = [process.env.FRONTEND_URL, 'http://localhost:5173']; // Allow local and production frontend origins
 
 export const corsOptions = {
   origin: origins,
