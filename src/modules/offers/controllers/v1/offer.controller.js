f4f0035d16d2d05de5ecb2b4a4487e40de15343e
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-07-11 23:18:33 +0430

    Add controller function #122

--- src/modules/offers/controllers/v1/offer.controller.js
+++ src/modules/offers/controllers/v1/offer.controller.js
@@ -1,10 +1,12 @@
 import {
   acceptAnOfferWithTransaction,
+  getCurrentOfferForDriver,
   rejectAnOfferWithTransaction,
 } from '../../services/v1/offer.service.js';
-import { unauthorized } from '#shared/errors/error.js';
+import { notFound, unauthorized } from '#shared/errors/error.js';
 import { OfferService } from '#shared/utils/offerService.js';
 import { DbJobService } from '#shared/utils/dbJob.service.js';
+import { fetchDriverByUserId } from '#modules/drivers/index.js';
 
 export const acceptOffer = async (req, res) => {
   if (!req.user) throw unauthorized();
@@ -19,5 +21,15 @@ export const rejectOffer = async (req, res) => {
   const offer = await rejectAnOfferWithTransaction(req.params.id, req.user._id);
   await OfferService.sendOfferToDriver(offer.order.toString());
   await DbJobService.scheduleCalculateAcceptanceRate(req.user._id);
+  res.status(200).json({ success: true, data: offer });
+};
+
+export const getCurrentOffer = async (req, res) => {
+  if (!req.user) throw unauthorized();
+
+  const driver = await fetchDriverByUserId(req.user._id);
+  if (!driver) throw notFound('Driver');
+
+  const offer = await getCurrentOfferForDriver(driver._id);
   res.status(200).json({ success: true, data: offer });
 };
