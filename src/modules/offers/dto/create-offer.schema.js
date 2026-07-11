e10f1ecf84776481309dcf848d3783d60ca75b22
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-07-11 23:18:58 +0430

    Add metadata to createOfferSchema  #122

--- src/modules/offers/dto/create-offer.schema.js
+++ src/modules/offers/dto/create-offer.schema.js
@@ -9,4 +9,5 @@ export const createOfferSchema = z.object({
   driver: z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
     message: ERROR_CODES.INVALID_DRIVER_ID,
   }),
+  metadata: z.any(),
 });
