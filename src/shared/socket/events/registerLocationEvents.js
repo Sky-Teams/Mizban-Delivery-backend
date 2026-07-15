ea980a29f2f216145da1573e2d8d746c52afb618
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-15 16:34:50 +0430

    add console log to give information about location update

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -3,5 +3,6 @@ import { LocationService } from '#shared/utils/locationService.js';
 export const registerLocationEvents = (socket) => {
   socket.on('update_location', async (data) => {
     await LocationService.updateLocation(socket.userId, data);
+    console.log(`Location updated successfully for driver: ${socket.userId}`);
   });
 };
