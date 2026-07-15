6d3efb8b995b3af61e70f6032348b11739494d84
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-15 16:35:24 +0430

    fix latitude and longitude bug

--- src/modules/locations/services/v1/location.service.js
+++ src/modules/locations/services/v1/location.service.js
@@ -7,13 +7,27 @@ export const updateDriverLocation = async (userId, data) => {
   let driver = await fetchDriverByUserId(userId);
   if (!driver) throw notFound('driver');
 
+  const [lat, long] = data.currentLocation.coordinates;
   /** Validation location data (type, latitude, longitude) */
-  updateLocationSchema.parse(data);
+  updateLocationSchema.parse({
+    currentLocation: {
+      type: data.currentLocation.type,
+      coordinates: [long, lat],
+    },
+  });
 
   /** Update driver location to DB */
   driver = await DriverModel.findOneAndUpdate(
     { _id: driver.id, user: userId },
-    { $set: { currentLocation: data.currentLocation, lastLocationAt: new Date() } },
+    {
+      $set: {
+        currentLocation: {
+          type: data.currentLocation.type,
+          coordinates: [long, lat],
+        },
+        lastLocationAt: new Date(),
+      },
+    },
     { new: true, runValidators: true }
   );
 
