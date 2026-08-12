1c9b484cdac973afc3fea70d8fe6a9d4c4d74488
fataneh-mow <fataneh.mow@gmail.com>
2026-08-12 13:35:35 +0430

    feat: schema for request implemented #125

---
+++ src/modules/orders/dto/calculate-delivery-price.schema.js
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const calculateDeliveryPriceSchema = z.object({
+  body: z.object({
+    pickupLocation: z.object({
+      coordinates: z.array(z.number()).length(2),
+    }),
+
+    dropoffLocation: z.object({
+      coordinates: z.array(z.number()).length(2),
+    }),
+  }),
+});
+
+export const calculateDeliveryPriceValidator = (req) => {
+  return calculateDeliveryPriceSchema.safeParse(req);
+}
