1252ecab831c9a219fa513aacd77aaa7ec51ad54
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-08-26 12:58:42 -0700

    Use driverId instead userId

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -2,7 +2,7 @@ import { LocationService } from '#shared/utils/locationService.js';
 
 export const registerLocationEvents = (socket) => {
   socket.on('update_location', async (data) => {
-    await LocationService.updateLocation(socket.userId, data);
-    console.log(`Location updated requested for driver: ${socket.userId}`);
+    const driver = await LocationService.updateLocation(socket.userId, data);
+    console.log(`Location updated requested for driver: ${driver._id}`);
   });
 };
