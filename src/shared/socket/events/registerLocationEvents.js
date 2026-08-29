3a7f1add02653f94161b60abfe7bdeebb20c02b2
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-08-29 11:55:57 -0700

    Check driver existing

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -3,6 +3,7 @@ import { LocationService } from '#shared/utils/locationService.js';
 export const registerLocationEvents = (socket) => {
   socket.on('update_location', async (data) => {
     const driver = await LocationService.updateLocation(socket.userId, data);
+    if (!driver) return;
     console.log(`Location updated requested for driver: ${driver._id}`);
   });
 };
