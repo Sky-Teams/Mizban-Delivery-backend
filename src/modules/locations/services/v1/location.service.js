66bcb4f6be481431b3f15986b619a39183435258
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-09-01 01:22:50 -0700

    Update driver location in db

--- src/modules/locations/services/v1/location.service.js
+++ src/modules/locations/services/v1/location.service.js
@@ -1,40 +1,22 @@
-import { fetchDriverByUserId } from '#modules/drivers/index.js';
 import { DriverModel } from '#modules/drivers/models/driver.model.js';
-import { updateLocationSchema } from '#modules/locations/dto/update-location.schema.js';
-import { ERROR_CODES } from '#shared/errors/customCodes.js';
-import { AppError, notFound } from '#shared/errors/error.js';
 
-export const updateDriverLocation = async (userId, data) => {
-  let driver = await fetchDriverByUserId(userId);
-  if (!driver) throw notFound('driver');
-
-  // Validate data
-  if (!data || !data.currentLocation.coordinates || data.currentLocation.coordinates.length < 2)
-    throw new AppError('Invalid data location', 400, ERROR_CODES.INVALID_LOCATION_DATA);
-
-  const [lat, long] = data.currentLocation.coordinates;
-  /** Validation location data (type, latitude, longitude) */
-  updateLocationSchema.parse({
-    currentLocation: {
-      type: data.currentLocation.type,
-      coordinates: [long, lat],
-    },
-  });
+export const updateDriverLocation = async (driverId, data) => {
+  const [lat, lng] = data.currentLocation.coordinates;
 
   /** Update driver location to DB */
-  driver = await DriverModel.findOneAndUpdate(
-    { _id: driver.id, user: userId },
+  let driver = await DriverModel.findOneAndUpdate(
+    { _id: driverId },
     {
       $set: {
         currentLocation: {
           type: data.currentLocation.type,
-          coordinates: [long, lat],
+          coordinates: [lng, lat],
         },
         lastLocationAt: new Date(),
       },
     },
-    { new: true, runValidators: true }
-  ).populate('user', 'name email phone');
+    { returnDocument: 'after', runValidators: true }
+  );
 
   return driver;
 };
