5d390b5507fb3b2ee80de8c796821a0b4818387f
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-09-01 01:23:26 -0700

    Implement redis and some functions in Location Service

--- src/shared/utils/locationService.js
+++ src/shared/utils/locationService.js
@@ -1,8 +1,17 @@
+import { fetchDriverByUserId } from '#modules/drivers/index.js';
+import { updateLocationSchema } from '#modules/locations/dto/update-location.schema.js';
 import { updateDriverLocation } from '#modules/locations/index.js';
+import { ERROR_CODES } from '#shared/errors/customCodes.js';
+import { AppError, notFound } from '#shared/errors/error.js';
 import { DtoService } from './dtoService.js';
 import { SOCKET_EVENTS } from './enums.js';
 import { NotificationPayloads } from './notificationPayloadBuilder.js';
 import { NotificationService } from './notificationService.js';
+import {
+  deleteDriverLocationFromRedis,
+  getDriverLocationFromRedis,
+  saveLocationInRedis,
+} from './redisLocationService.js';
 
 export class LocationService {
   /**
@@ -12,16 +21,39 @@ export class LocationService {
    */
   static async updateLocation(userId, data) {
     try {
-      /** TODO: Implement Redis caching in future */
+      let driver = await fetchDriverByUserId(userId);
+      if (!driver) throw notFound('driver');
 
-      let driver = await updateDriverLocation(userId, data);
+      // Validate data
+      if (!data || !data.currentLocation.coordinates || data.currentLocation.coordinates.length < 2)
+        throw new AppError('Invalid data location', 400, ERROR_CODES.INVALID_LOCATION_DATA);
 
-      const filterDriver = await DtoService.formatDriver(driver);
+      const [lat, lng] = data.currentLocation.coordinates;
+      /** Validation location data (type, latitude, longitude) */
+      updateLocationSchema.parse({
+        currentLocation: {
+          type: data.currentLocation.type,
+          coordinates: [lng, lat],
+        },
+      });
+
+      /** Save locations in Redis caching  */
+      await saveLocationInRedis(driver._id, [lat, lng]);
+
+      // Get drivers location from redis
+      const location = await getDriverLocationFromRedis(driver._id);
+      const filtered = await DtoService.formatDriver(driver);
       /** Emit LOCATION_UPDATED to admin */
       await NotificationService.send(
         'admins',
         SOCKET_EVENTS.DRIVER.LOCATION_UPDATED,
-        filterDriver,
+        {
+          ...filtered,
+          currentLocation: {
+            type: 'Point',
+            coordinates: [Number(location.lat), Number(location.lng)],
+          },
+        },
         userId,
         false
       );
@@ -83,6 +115,83 @@ export class LocationService {
       );
 
       return;
+    }
+  }
+
+  /**
+   *  Start Driver Tracking
+   * @param {string} userId
+   * @returns {object}
+   */
+  static async startDriverTracking(userId) {
+    try {
+      const driver = await fetchDriverByUserId(userId);
+      if (!driver) throw notFound('driver');
+
+      return driver;
+    } catch (error) {
+      /** If the driver is not found, notify the driver and stop the location update process */
+      if (error.status === 404) {
+        console.warn(error);
+
+        const payload = {
+          code: 'DRIVER_NOT_FOUND',
+          message: error.message,
+        };
+        await NotificationService.send(
+          'driver',
+          SOCKET_EVENTS.DRIVER.LOCATION_ERROR,
+          payload,
+          userId,
+          false
+        );
+        return;
+      }
+    }
+  }
+
+  /**
+   * Stop Driver Tracking And Remove Driver Locations From Redis
+   * @param {string} userId
+   * @returns {string}
+   */
+  static async stopDriverTracking(userId) {
+    try {
+      const driver = await fetchDriverByUserId(userId);
+      if (!driver) throw notFound('driver');
+
+      /** Save Last Location in DB */
+      const location = await getDriverLocationFromRedis(driver._id);
+      const data = {
+        currentLocation: {
+          type: 'Point',
+          coordinates: [Number(location.lat), Number(location.lng)],
+        },
+      };
+      await updateDriverLocation(driver._id, data);
+
+      /** Remove Driver Location From Redis */
+      await deleteDriverLocationFromRedis(driver._id);
+
+      return driver;
+    } catch (error) {
+      /** If the driver is not found, notify the driver and stop the location update process */
+      if (error.status === 404) {
+        console.warn(error);
+
+        const payload = {
+          code: 'DRIVER_NOT_FOUND',
+          message: error.message,
+        };
+        await NotificationService.send(
+          'driver',
+          SOCKET_EVENTS.DRIVER.LOCATION_ERROR,
+          payload,
+          userId,
+          false
+        );
+        return;
+      }
     }
   }
 }
