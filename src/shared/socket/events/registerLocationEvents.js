459b2f93e951bbe0b923dcc3bca7034baa544b99
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-09-01 01:24:18 -0700

    Add more socket event

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -1,9 +1,23 @@
+import { startLocationSync, stopLocationSync } from '../../../jobs/locationSync.js';
 import { LocationService } from '#shared/utils/locationService.js';
 
 export const registerLocationEvents = (socket) => {
+  socket.on('start_tracking', async () => {
+    const driver = await LocationService.startDriverTracking(socket.userId);
+
+    /** Start Location Sync With Database */
+    startLocationSync(driver._id);
+  });
+
   socket.on('update_location', async (data) => {
-    const driver = await LocationService.updateLocation(socket.userId, data);
-    if (!driver) return;
-    console.log(`Location updated requested for driver: ${driver._id}`);
+    /** Update Driver Location To Redis and Send Driver Location to Admin */
+    await LocationService.updateLocation(socket.userId, data);
+  });
+
+  socket.on('stop_tracking', async () => {
+    const driver = await LocationService.stopDriverTracking(socket.userId);
+    
+    /** Stop Location Saving In Database */
+    stopLocationSync(driver._id);
   });
 };
