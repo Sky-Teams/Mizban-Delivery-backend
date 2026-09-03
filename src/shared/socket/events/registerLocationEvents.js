0f83175282eaee46e86bf6be02f8356796a83269
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-09-03 12:13:25 -0700

    Use SOCKET_EVENTS enum

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -1,22 +1,23 @@
 import { startLocationSync, stopLocationSync } from '../../../jobs/locationSync.js';
 import { LocationService } from '#shared/utils/locationService.js';
+import { SOCKET_EVENTS } from '#shared/utils/enums.js';
 
 export const registerLocationEvents = (socket) => {
-  socket.on('start_tracking', async () => {
+  socket.on(SOCKET_EVENTS.DRIVER.START_TRACKING, async () => {
     const driver = await LocationService.startDriverTracking(socket.userId);
 
     /** Start Location Sync With Database */
     startLocationSync(driver._id);
   });
 
-  socket.on('update_location', async (data) => {
+  socket.on(SOCKET_EVENTS.DRIVER.UPDATE_LOCATION, async (data) => {
     /** Update Driver Location To Redis and Send Driver Location to Admin */
     await LocationService.updateLocation(socket.userId, data);
   });
 
-  socket.on('stop_tracking', async () => {
+  socket.on(SOCKET_EVENTS.DRIVER.STOP_TRACKING, async () => {
     const driver = await LocationService.stopDriverTracking(socket.userId);
-    
+
     /** Stop Location Saving In Database */
     stopLocationSync(driver._id);
   });
