0bac9bbd6a91b4c153a19b11aab9b085e792966d
Ebrahimi <sakina.ibrahiey986@gmail.com>
2026-09-06 00:14:28 -0700

    Add callback in start tracking socket event

--- src/shared/socket/events/registerLocationEvents.js
+++ src/shared/socket/events/registerLocationEvents.js
@@ -1,13 +1,11 @@
-import { startLocationSync, stopLocationSync } from '../../../jobs/locationSync.js';
 import { LocationService } from '#shared/utils/locationService.js';
 import { SOCKET_EVENTS } from '#shared/utils/enums.js';
 
 export const registerLocationEvents = (socket) => {
-  socket.on(SOCKET_EVENTS.DRIVER.START_TRACKING, async () => {
-    const driver = await LocationService.startDriverTracking(socket.userId);
-
-    /** Start Location Sync With Database */
-    startLocationSync(driver._id);
+  socket.on(SOCKET_EVENTS.DRIVER.START_TRACKING, async (callback) => {
+    /** Save location in DB */
+    const result = await LocationService.startDriverTracking(socket.userId);
+    callback(result);
   });
 
   socket.on(SOCKET_EVENTS.DRIVER.UPDATE_LOCATION, async (data) => {
@@ -16,9 +14,7 @@ export const registerLocationEvents = (socket) => {
   });
 
   socket.on(SOCKET_EVENTS.DRIVER.STOP_TRACKING, async () => {
-    const driver = await LocationService.stopDriverTracking(socket.userId);
-
-    /** Stop Location Saving In Database */
-    stopLocationSync(driver._id);
+    /** Save last location in DB and stop tracking */
+    await LocationService.stopDriverTracking(socket.userId);
   });
 };
